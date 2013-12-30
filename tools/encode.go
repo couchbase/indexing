393a088126930e2662ff267c00d65a04a76562b8
prataprc <prataprc@gmail.com>
2013-12-30 15:45:58 +0530

    Added integer encoding switch.

--- tools/encode.go
+++ tools/encode.go
@@ -9,6 +9,7 @@ import (
 
 var options struct {
     floatText string
+    intText string
 }
 
 func argParse() {
@@ -17,6 +18,7 @@ func argParse() {
     //flag.IntVar(&options.count, "n", 1, "Generate n combinations")
     //flag.StringVar(&options.outfile, "o", "-", "Specify an output file")
     flag.StringVar(&options.floatText, "f", "", "encode floating point number")
+    flag.StringVar(&options.intText, "i", "", "encode integer number")
     flag.Parse()
 }
 
@@ -24,6 +26,8 @@ func main() {
     argParse()
     if options.floatText != "" {
         encodeFloat(options.floatText)
+    } else if options.intText != "" {
+        encodeInt(options.intText)
     }
 }
 
@@ -34,4 +38,8 @@ func encodeFloat(text string) {
         ftext := []byte(strconv.FormatFloat(f, 'e', -1, 64))
         fmt.Printf("Encoding %v: %v\n", f, string(collatejson.EncodeFloat(ftext)))
     }
+}
+
+func encodeInt(text string) {
+    fmt.Println(string(collatejson.EncodeInt([]byte(text))))
 }
