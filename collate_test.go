d57c791c7bf9c8cdf4f6dca591096241f6db0b2f
prataprc <prataprc@gmail.com>
2014-01-03 15:09:11 +0530

    Added benchmarks for simple data types.
    - benchmarking tuqcollate for simple data types.
    - benchmarking encoding/json:Unmarshal() & reflect:DeepEqual()
    both of them are under dpcollate_test.go
    
    Go formating rules applied.

--- collate_test.go
+++ collate_test.go
@@ -1,15 +1,15 @@
 package collatejson
 
 import (
-    "bytes"
-    "fmt"
-    "testing"
-    "encoding/json"
-    "reflect"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"testing"
 )
 
 var json1 = []byte(
-    `{ "inelegant":27.53096820876087,
+	`{ "inelegant":27.53096820876087,
 "horridness":true,
 "iridodesis":[79.1253026404128,null],
 "arrogantness":null,
@@ -17,49 +17,49 @@ var json1 = []byte(
 }`)
 
 func TestEncode(t *testing.T) {
-    codec := NewCodec()
-    code := codec.Encode(json1)
-    ref := `\b` +
-        `\x05>5\x00` +
-        `\x06arrogantness\x00\x02\x00` +
-        `\x06horridness\x00\x04\x00` +
-        `\x06inelegant\x00\x05>>22753096820876087-\x00` +
-        `\x06iridodesis\x00\a\x05>2\x00\x05>>2791253026404128-\x00\x02\x00\x00\x06` +
-        `unagrarian\x00\x03\x00\x00`
+	codec := NewCodec()
+	code := codec.Encode(json1)
+	ref := `\b` +
+		`\x05>5\x00` +
+		`\x06arrogantness\x00\x02\x00` +
+		`\x06horridness\x00\x04\x00` +
+		`\x06inelegant\x00\x05>>22753096820876087-\x00` +
+		`\x06iridodesis\x00\a\x05>2\x00\x05>>2791253026404128-\x00\x02\x00\x00\x06` +
+		`unagrarian\x00\x03\x00\x00`
 
-    out := fmt.Sprintf("%q", code)
-    out = out[1 : len(out)-1]
-    if out != ref {
-        t.Error("Encode fails, did you change the encoding format ?")
-    }
+	out := fmt.Sprintf("%q", code)
+	out = out[1 : len(out)-1]
+	if out != ref {
+		t.Error("Encode fails, did you change the encoding format ?")
+	}
 }
 
 func TestDecode(t *testing.T) {
-    var one, two map[string]interface{}
+	var one, two map[string]interface{}
 
-    codec := NewCodec()
-    out := codec.Decode(codec.Encode(json1))
+	codec := NewCodec()
+	out := codec.Decode(codec.Encode(json1))
 
-    json.Unmarshal(json1, &one)
-    json.Unmarshal(out, &two)
-    if !reflect.DeepEqual(one, two) {
-        t.Error("Decode fails, did you change the encoding format ?")
-    }
+	json.Unmarshal(json1, &one)
+	json.Unmarshal(out, &two)
+	if !reflect.DeepEqual(one, two) {
+		t.Error("Decode fails, did you change the encoding format ?")
+	}
 }
 
 func BenchmarkEncode(b *testing.B) {
-    codec := NewCodec()
-    b.ResetTimer()
-    for i := 0; i < b.N; i++ {
-        codec.Encode(json1)
-    }
+	codec := NewCodec()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		codec.Encode(json1)
+	}
 }
 
 func BenchmarkCompare(b *testing.B) {
-    codec := NewCodec()
-    code := codec.Encode(json1)
-    b.ResetTimer()
-    for i := 0; i < b.N; i++ {
-        bytes.Compare(code, code)
-    }
+	codec := NewCodec()
+	code := codec.Encode(json1)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		bytes.Compare(code, code)
+	}
 }
--- dpcollate_test.go
+++ dpcollate_test.go
@@ -1,8 +1,10 @@
 package collatejson
 
 import (
+	"encoding/json"
 	"github.com/couchbaselabs/dparval"
 	tuqcollate "github.com/couchbaselabs/tuqtng/ast"
+	"reflect"
 	"testing"
 )
 
@@ -13,7 +15,44 @@ func BenchmarkDParVal(b *testing.B) {
 	}
 }
 
-func BenchmarkTuqCollate(b *testing.B) {
+func BenchmarkTuqCollateInt(b *testing.B) {
+	jsonb := []byte(`1234567890`)
+	key1 := dparval.NewValueFromBytes(jsonb)
+	value1 := key1.Value()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		key2 := dparval.NewValueFromBytes(jsonb)
+		value2 := key2.Value()
+		tuqcollate.CollateJSON(value1, value2)
+	}
+}
+
+func BenchmarkTuqCollateFloat(b *testing.B) {
+	jsonb := []byte(`1234567890.001234556`)
+	key1 := dparval.NewValueFromBytes(jsonb)
+	value1 := key1.Value()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		key2 := dparval.NewValueFromBytes(jsonb)
+		value2 := key2.Value()
+		tuqcollate.CollateJSON(value1, value2)
+	}
+}
+
+func BenchmarkTuqCollateArray(b *testing.B) {
+	jsonb := []byte(
+		`[123456789, 123456789.1234567879, "hello world", true, false, null]`)
+	key1 := dparval.NewValueFromBytes(jsonb)
+	value1 := key1.Value()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		key2 := dparval.NewValueFromBytes(jsonb)
+		value2 := key2.Value()
+		tuqcollate.CollateJSON(value1, value2)
+	}
+}
+
+func BenchmarkTuqCollateMap(b *testing.B) {
 	key1 := dparval.NewValueFromBytes(json1)
 	value1 := key1.Value()
 	b.ResetTimer()
@@ -21,5 +60,49 @@ func BenchmarkTuqCollate(b *testing.B) {
 		key2 := dparval.NewValueFromBytes(json1)
 		value2 := key2.Value()
 		tuqcollate.CollateJSON(value1, value2)
+	}
+}
+
+func BenchmarkJsonInt(b *testing.B) {
+	var value1, value2 interface{}
+	jsonb := []byte(`1234567890`)
+	json.Unmarshal([]byte(jsonb), &value1)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		json.Unmarshal([]byte(jsonb), &value2)
+		reflect.DeepEqual(value1, value2)
+	}
+}
+
+func BenchmarkJsonFloat(b *testing.B) {
+	var value1, value2 interface{}
+	jsonb := []byte(`1234567890.001234556`)
+	json.Unmarshal([]byte(jsonb), &value1)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		json.Unmarshal([]byte(jsonb), &value2)
+		reflect.DeepEqual(value1, value2)
+	}
+}
+
+func BenchmarkJsonArray(b *testing.B) {
+	var value1, value2 interface{}
+	jsonb := []byte(
+		`[123456789, 123456789.1234567879, "hello world", true, false, null]`)
+	json.Unmarshal([]byte(jsonb), &value1)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		json.Unmarshal([]byte(jsonb), &value2)
+		reflect.DeepEqual(value1, value2)
+	}
+}
+
+func BenchmarkJsonMap(b *testing.B) {
+	var value1, value2 interface{}
+	json.Unmarshal([]byte(json1), &value1)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		json.Unmarshal([]byte(json1), &value2)
+		reflect.DeepEqual(value1, value2)
 	}
 }
