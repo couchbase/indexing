ec7cc4fb56784e30c91f86adfa42e43d8f63e429
prataprc <prataprc@gmail.com>
2014-04-11 19:22:39 +0530

    common/mutation.go (KeyVersions, VbConnectionMap)
    - Mutation message can carry either KeyVersions or, Vbucket list that is bound
      to the connection.
    - KeyVersions for different vbucket (but bounded to same connection) will be
      grouped together as payload for a single Mutation message.
    - Added DropData command.
    
    Change-Id: I9ec731bd3207a5da7b0f783e7d45100325f87f6f
    Reviewed-on: http://review.couchbase.org/35603
    Reviewed-by: Pratap Chakravarthy <prataprc@couchbase.com>
    Tested-by: Pratap Chakravarthy <prataprc@couchbase.com>

--- secondary/common/mutation.go
+++ secondary/common/mutation.go
@@ -7,7 +7,13 @@ package common
 
 import (
 	"code.google.com/p/goprotobuf/proto"
+	"fmt"
 	"github.com/couchbase/indexing/secondary/protobuf"
+)
+
+const (
+	PAYLOAD_KEYVERSIONS byte = iota + 1
+	PAYLOAD_VBMAP
 )
 
 // List of possible mutation commands. Mutation messages are broadly divided
@@ -17,67 +23,139 @@ const (
 	Deletion                       // data command
 	UpsertDeletion                 // data command
 	Sync                           // control command
+	DropData                       // control command
 	StreamBegin                    // control command
 	StreamEnd                      // control command
 )
 
-// Mutation structure describing KV mutations.
-type Mutation struct {
-	// Required fields
-	Version byte // protocol version: TBD
-	Command byte
-	Vbucket uint16 // vbucket number
-	Vbuuid  uint64 // unique id to detect branch history
-
-	// Optional fields
-	Docid    []byte   // primary document id
+// KeyVersions for each mutation from KV for a subset of index.
+type KeyVersions struct {
+	Command  byte
+	Vbucket  uint16   // vbucket number
 	Seqno    uint64   // vbucket sequence number for this mutation
+	Vbuuid   uint64   // unique id to detect branch history
+	Docid    []byte   // primary document id
 	Keys     [][]byte // list of key-versions
 	Oldkeys  [][]byte // previous key-versions, if available
 	Indexids []uint32 // each key-version is for an active index defined on this bucket.
+}
+
+// Mutation message either carrying KeyVersions or protobuf.VbConnectionMap
+type Mutation struct {
+	version    byte // protocol Version: TBD
+	keys       []*KeyVersions
+	vbuckets   []uint16
+	vbuuids    []uint64
+	payltyp    byte
+	maxKeyvers int
+}
+
+func NewMutation(maxKeyvers int) *Mutation {
+	m := &Mutation{
+		version:    ProtobufVersion(),
+		keys:       make([]*KeyVersions, 0, maxKeyvers),
+		maxKeyvers: maxKeyvers,
+	}
+	return m
+}
+
+func (m *Mutation) NewPayload(payltyp byte) {
+	m.keys = m.keys[:0]
+	m.payltyp = payltyp
+}
+
+func (m *Mutation) AddKeyVersions(k *KeyVersions) (err error) {
+	if m.payltyp != PAYLOAD_KEYVERSIONS {
+		return fmt.Errorf("expected key version for payload")
+	} else if len(m.keys) == m.maxKeyvers {
+		return fmt.Errorf("cannot pack anymore key-versions")
+	}
+	m.keys = append(m.keys, k)
+	return nil
+}
+
+func (m *Mutation) SetVbuckets(vbuckets []uint16, vbuuids []uint64) (err error) {
+	if m.payltyp != PAYLOAD_VBMAP {
+		return fmt.Errorf("expected key version for payload")
+	}
+	m.vbuckets = vbuckets
+	m.vbuuids = vbuuids
+	return nil
+}
+
+func (m *Mutation) GetKeyVersion() []*KeyVersions {
+	return m.keys
 }
 
 // Encode Mutation structure into protobuf array of bytes. Returned `data` can
 // be transported to the other end and decoded back to Mutation structure.
 func (m *Mutation) Encode() (data []byte, err error) {
 	mp := protobuf.Mutation{
-		Version: proto.Uint32(uint32(m.Version)),
-		Command: proto.Uint32(uint32(m.Command)),
-		Vbucket: proto.Uint32(uint32(m.Vbucket)),
-		Vbuuid:  proto.Uint64(uint64(m.Vbuuid)),
+		Version: proto.Uint32(uint32(m.version)),
 	}
-	if m.Docid != nil && len(m.Docid) > 0 {
-		mp.Docid = m.Docid
+
+	switch m.payltyp {
+	case PAYLOAD_KEYVERSIONS:
+		mp.Keys = make([]*protobuf.KeyVersions, 0, len(m.keys))
+		if len(m.keys) == 0 {
+			err = fmt.Errorf("empty mutation")
+			break
+		}
+		for _, k := range m.keys {
+			kp := &protobuf.KeyVersions{
+				Command: proto.Uint32(uint32(k.Command)),
+				Vbucket: proto.Uint32(uint32(k.Vbucket)),
+				Vbuuid:  proto.Uint64(uint64(k.Vbuuid)),
+			}
+			if k.Docid != nil && len(k.Docid) > 0 {
+				kp.Docid = k.Docid
+			}
+			if k.Seqno > 0 {
+				kp.Seqno = proto.Uint64(k.Seqno)
+			}
+			if k.Keys != nil {
+				kp.Keys = k.Keys
+				kp.Oldkeys = k.Oldkeys
+				kp.Indexids = k.Indexids
+			}
+			mp.Keys = append(mp.Keys, kp)
+		}
+	case PAYLOAD_VBMAP:
+		vbuckets := make([]uint32, 0, len(m.vbuckets))
+		for _, vb := range m.vbuckets {
+			vbuckets = append(vbuckets, uint32(vb))
+		}
+		mp.Vbuckets = &protobuf.VbConnectionMap{
+			Vbuuids:  m.vbuuids,
+			Vbuckets: vbuckets,
+		}
 	}
-	if m.Seqno > 0 {
-		mp.Seqno = proto.Uint64(m.Seqno)
+
+	if err == nil {
+		data, err = proto.Marshal(&mp)
 	}
-	if m.Keys != nil {
-		mp.Keys = m.Keys
-		mp.Oldkeys = m.Oldkeys
-		mp.Indexids = m.Indexids
-	}
-	data, err = proto.Marshal(&mp)
 	return
 }
 
 // Decode complements Encode() API. `data` returned by encode can be converted
-// back to Mutation structure.
-func (m *Mutation) Decode(data []byte) (err error) {
+// back to either *protobuf.VbConnectionMap, or []*protobuf.KeyVersions
+func (m *Mutation) Decode(data []byte) (interface{}, error) {
+	var err error
+
 	mp := protobuf.Mutation{}
 	if err = proto.Unmarshal(data, &mp); err != nil {
-		return
+		return nil, err
 	}
-	m.Version = byte(mp.GetVersion())
-	m.Command = byte(mp.GetCommand())
-	m.Vbucket = uint16(mp.GetVbucket())
-	m.Vbuuid = mp.GetVbuuid()
-	m.Docid = mp.GetDocid()
-	m.Seqno = mp.GetSeqno()
-	m.Keys = mp.GetKeys()
-	m.Oldkeys = mp.GetOldkeys()
-	m.Indexids = mp.GetIndexids()
-	return
+	if ver := byte(mp.GetVersion()); ver != ProtobufVersion() {
+		return nil, fmt.Errorf("mismatch in transport version %v", ver)
+	}
+
+	if vbuckets := mp.GetVbuckets(); vbuckets != nil {
+		return vbuckets, nil
+	} else if keys := mp.GetKeys(); keys != nil {
+		return keys, nil
+	}
+	return nil, fmt.Errorf("mutation does not have payload")
 }
 
 // TBD: Yet to be defined. Just a place holder for now.
