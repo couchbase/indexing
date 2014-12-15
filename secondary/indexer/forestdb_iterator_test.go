a166da7eb6b5897c23ecab27a2517267c0f9270e
deepkaran <deepkaran.salooja@gmail.com>
2014-12-15 14:31:24 +0530

    ForestDB Iterator Changes
    ForestDB Iterator changes required due to change in ForestDB API
    http://review.couchbase.org/#/c/42859/
    
    corresponding goforestdb changes are already in master:
    https://github.com/couchbaselabs/goforestdb/commit/3540c6f6a372e1af2cf40a68a7a28207d5e0afd8
    
    Currently sherlock manifest is locked down on a stable commit which does
    not include the new changes for ForestDB/goforestdb. This commit can only be
    merged once the manifest starts pointing to latest changes.
    
    Added test file for validation as well.
    
    Change-Id: I7bfe003824c6303102bac0498f73d2eea61919fd

---
+++ secondary/indexer/forestdb_iterator_test.go
@@ -0,0 +1,143 @@
+package indexer
+
+import (
+	forestdb "github.com/couchbaselabs/goforestdb"
+	"os"
+	"testing"
+)
+
+func TestForestDBIterator(t *testing.T) {
+	defer os.RemoveAll("test")
+
+	dbfile, err := forestdb.Open("test", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer dbfile.Close()
+
+	kvstore, err := dbfile.OpenKVStoreDefault(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer kvstore.Close()
+
+	// store a bunch of values to test the iterator
+
+	kvstore.SetKV([]byte("a"), []byte("vala"))
+	kvstore.SetKV([]byte("b"), []byte("valb"))
+	kvstore.SetKV([]byte("c"), []byte("valc"))
+	kvstore.SetKV([]byte("d"), []byte("vald"))
+	kvstore.SetKV([]byte("e"), []byte("vale"))
+	kvstore.SetKV([]byte("f"), []byte("valf"))
+	kvstore.SetKV([]byte("g"), []byte("valg"))
+	kvstore.SetKV([]byte("h"), []byte("valh"))
+	kvstore.SetKV([]byte("i"), []byte("vali"))
+	kvstore.SetKV([]byte("j"), []byte("valj"))
+
+	dbfile.Commit(forestdb.COMMIT_MANUAL_WAL_FLUSH)
+
+	info, err := kvstore.Info()
+	if err != nil {
+		t.Fatal(err)
+	}
+	lastSeqNum := info.LastSeqNum()
+
+	iter, err := newForestDBIterator(kvstore, lastSeqNum)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer iter.Close()
+
+	iter.SeekFirst()
+
+	count := 0
+	var firstKey, lastKey []byte
+
+	for ; iter.Valid(); iter.Next() {
+		if firstKey == nil {
+			firstKey = iter.Key()
+		}
+		count++
+		lastKey = iter.Key()
+	}
+
+	if count != 10 {
+		t.Errorf("exptected to iterate 10, saw %d", count)
+	}
+	if string(firstKey) != "a" {
+		t.Errorf("expected fist key to be a, got %s", firstKey)
+	}
+	if string(lastKey) != "j" {
+		t.Errorf("expected last key to be j, got %s", lastKey)
+	}
+
+}
+
+func TestForestDBIteratorSeek(t *testing.T) {
+	defer os.RemoveAll("test")
+
+	dbfile, err := forestdb.Open("test", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer dbfile.Close()
+
+	kvstore, err := dbfile.OpenKVStoreDefault(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer kvstore.Close()
+
+	// store a bunch of values to test the iterator
+
+	kvstore.SetKV([]byte("a"), []byte("vala"))
+	kvstore.SetKV([]byte("b"), []byte("valb"))
+	kvstore.SetKV([]byte("c"), []byte("valc"))
+	kvstore.SetKV([]byte("d"), []byte("vald"))
+	kvstore.SetKV([]byte("e"), []byte("vale"))
+	kvstore.SetKV([]byte("f"), []byte("valf"))
+	kvstore.SetKV([]byte("g"), []byte("valg"))
+	kvstore.SetKV([]byte("h"), []byte("valh"))
+	kvstore.SetKV([]byte("i"), []byte("vali"))
+	kvstore.SetKV([]byte("j"), []byte("valj"))
+
+	dbfile.Commit(forestdb.COMMIT_MANUAL_WAL_FLUSH)
+
+	info, err := kvstore.Info()
+	if err != nil {
+		t.Fatal(err)
+	}
+	lastSeqNum := info.LastSeqNum()
+
+	iter, err := newForestDBIterator(kvstore, lastSeqNum)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer iter.Close()
+
+	iter.Seek([]byte("d"))
+
+	count := 0
+	var firstKey, lastKey []byte
+
+	for ; iter.Valid(); iter.Next() {
+		if firstKey == nil {
+			firstKey = iter.Key()
+		}
+		count++
+		lastKey = iter.Key()
+	}
+
+	if count != 7 {
+		t.Errorf("exptected to iterate 7, saw %d", count)
+	}
+	if string(firstKey) != "d" {
+		t.Errorf("expected fist key to be d, got %s", firstKey)
+	}
+	if string(lastKey) != "j" {
+		t.Errorf("expected last key to be j, got %s", lastKey)
+	}
+
+}
