6a585f8466ec1a4a187a30f73d88bf9005c4101f
prataprc <prataprc@gmail.com>
2014-12-22 21:38:47 +0530

    Improved failoverlog.go script.
    Change-Id: I945199cd3776823825eee268098f755988b06fca

--- secondary/dcp/examples/failoverlog/failoverlog.go
+++ secondary/dcp/examples/failoverlog/failoverlog.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/couchbaselabs/go-couchbase"
@@ -12,18 +14,45 @@ import (
 const clusterAddr = "http://localhost:9000"
 
 var options struct {
+	bucketn     string
 	clusterAddr string
+	maxVbs      int
+	vbuckets    []uint16
 	repeat      int
 	repeatWait  int
 }
 
 func argParse() {
+	var vbuckets string
+
+	flag.StringVar(&options.bucketn, "bucket", "default",
+		"bucket name")
+	flag.IntVar(&options.maxVbs, "maxvbs", 1024,
+		"max configured vbuckets")
+	flag.StringVar(&vbuckets, "vbuckets", "",
+		"list of vbuckets to fetch flogs")
 	flag.IntVar(&options.repeat, "repeat", 1,
 		"number of time to repeat fetching failover log")
 	flag.IntVar(&options.repeatWait, "repeatWait", 1000,
 		"time in mS, to wait before next repeat")
 
 	flag.Parse()
+
+	vbnos := make([]uint16, 0, options.maxVbs)
+	if vbuckets == "" {
+		for i := 0; i < options.maxVbs; i++ {
+			vbnos = append(vbnos, uint16(i))
+		}
+	} else {
+		for _, a := range strings.Split(vbuckets, ",") {
+			vbno, err := strconv.Atoi(a)
+			if err != nil {
+				log.Fatal(err)
+			}
+			vbnos = append(vbnos, uint16(vbno))
+		}
+	}
+	options.vbuckets = vbnos
 
 	args := flag.Args()
 	if len(args) < 1 {
@@ -36,14 +65,14 @@ func argParse() {
 func main() {
 	argParse()
 	// get a bucket and mc.Client connection
-	bucket, err := getTestConnection("default")
+	bucket, err := getTestConnection(options.bucketn)
 	if err != nil {
 		panic(err)
 	}
 
 	// Get failover log for a vbucket
 	for options.repeat > 0 {
-		flogs, err := bucket.GetFailoverLogs([]uint16{0, 1, 2, 3, 4, 5, 6, 7})
+		flogs, err := bucket.GetFailoverLogs(options.vbuckets)
 		if err != nil {
 			panic(err)
 		}
