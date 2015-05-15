b62f71267c129989e965eba55c1114e4c0419004
Prathibha <prathibha.bisarahalli@gmail.com>
2015-05-15 13:09:00 +0530

    Tests - Temp change to Force panic on index scan results mismatch in TestLargeMutations
    Change-Id: I1569df460acdff93eee19e80d895252c95ea8f4e

--- secondary/tests/functionaltests/mutations_test.go
+++ secondary/tests/functionaltests/mutations_test.go
@@ -261,6 +261,7 @@ func TestLargeMutations(t *testing.T) {
 		scanResults, err = secondaryindex.ScanAll(index1, bucketName, indexScanAddress, defaultlimit, c.SessionConsistency, nil)
 		FailTestIfError(err, "Error in scan", t)
 		err = tv.Validate(docScanResults, scanResults)
+		tc.HandleError(err, "Scan 1: Error in scan result validation")
 		FailTestIfError(err, "Error in scan result validation", t)
 		log.Printf("Len of expected and actual scan results are :  %d and %d", len(docScanResults), len(scanResults))
 
@@ -268,6 +269,7 @@ func TestLargeMutations(t *testing.T) {
 		scanResults, err = secondaryindex.ScanAll(index2, bucketName, indexScanAddress, defaultlimit, c.SessionConsistency, nil)
 		FailTestIfError(err, "Error in scan", t)
 		err = tv.Validate(docScanResults, scanResults)
+		tc.HandleError(err, "Scan 2: Error in scan result validation")
 		FailTestIfError(err, "Error in scan result validation", t)
 		log.Printf("Len of expected and actual scan results are :  %d and %d", len(docScanResults), len(scanResults))
 
