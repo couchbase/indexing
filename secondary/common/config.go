e24292ebcb215a94f6f2352bd02a52ff930ba74e
prataprc <prataprc@gmail.com>
2015-09-28 16:24:45 +0530

    MB-16323: adjust projector & dataport timeouts.
    increase the timeouts to 300s to handle severly under-provisioned
    nodes, like having 4 cores and 35 indexes defined on 10 buckets
    hosted by 3 KV nodes.
    
    Change-Id: I9dc8946946f270dfa0245c45ab56bacd6332ee80

--- secondary/common/config.go
+++ secondary/common/config.go
@@ -29,10 +29,10 @@ import "runtime"
 //      "indexer.dataport.tcpReadDeadline",
 //
 // configurations for underprovisioned nodes,
-//	"projector.feedWaitStreamReqTimeout": 300 * 1000,
-//	"projector.feedWaitStreamEndTimeout": 300 * 1000,
-//	"projector.dataport.harakiriTimeout": 300 * 1000,
-//	"indexer.dataport.tcpReadDeadline": 300 * 1000
+//		"projector.feedWaitStreamReqTimeout": 300 * 1000,
+//		"projector.feedWaitStreamEndTimeout": 300 * 1000,
+//		"projector.dataport.harakiriTimeout": 300 * 1000,
+//		"indexer.dataport.tcpReadDeadline": 300 * 1000
 
 // formula to compute the default CPU allocation for projector.
 var projector_maxCpuPercent = (1 + (runtime.NumCPU() / 6)) * 100
@@ -107,15 +107,15 @@ var SystemConfig = Config{
 		true, // immutable
 	},
 	"projector.feedWaitStreamReqTimeout": ConfigValue{
-		10 * 1000,
+		300 * 1000,
 		"timeout, in milliseconds, to await a response for StreamRequest",
-		10 * 1000,
+		300 * 1000,
 		false, // mutable
 	},
 	"projector.feedWaitStreamEndTimeout": ConfigValue{
-		10 * 1000,
+		300 * 1000,
 		"timeout, in milliseconds, to await a response for StreamEnd",
-		10 * 1000,
+		300 * 1000,
 		false, // mutable
 	},
 	"projector.mutationChanSize": ConfigValue{
@@ -270,13 +270,13 @@ var SystemConfig = Config{
 		false, // mutable
 	},
 	"projector.dataport.harakiriTimeout": ConfigValue{
-		30 * 1000,
+		300 * 1000,
 		"timeout in milliseconds, after which endpoint will commit harakiri " +
 			"if not active, does not affect existing feeds, " +
 			"also refer to projector.adminport.readTimeout and " +
 			"indexer.dataport.tcpReadDeadline.",
-		30 * 1000, //10s
-		false,     // mutable
+		300 * 1000, //10s
+		false,      // mutable
 	},
 	"projector.dataport.maxPayload": ConfigValue{
 		1024 * 1024,
@@ -324,12 +324,12 @@ var SystemConfig = Config{
 		true,        // immutable
 	},
 	"indexer.dataport.tcpReadDeadline": ConfigValue{
-		30 * 1000,
+		300 * 1000,
 		"timeout, in milliseconds, while reading from socket, " +
 			"also refer to projector.adminport.readTimeout and " +
 			"projector.dataport.harakiriTimeout.",
-		30 * 1000, // 10s
-		true,      // immutable
+		300 * 1000, // 10s
+		false,      // mutable
 	},
 	// indexer queryport configuration
 	"indexer.queryport.maxPayload": ConfigValue{
