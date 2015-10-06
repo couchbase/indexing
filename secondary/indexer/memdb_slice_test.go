772d61b5892a516f55b6de41782a98d42487e3fe
Sarath Lakshman <sarathlakshman@slynux.com>
2015-10-06 12:47:16 +0530

    MB-16194 memdb: Add snapshots in memdb slice test
    Change-Id: I003448564c1e4fcc07ebb48482b944b4c53b0a8b

--- secondary/indexer/memdb_slice_test.go
+++ secondary/indexer/memdb_slice_test.go
@@ -3,16 +3,24 @@ package indexer
 import (
 	"fmt"
 	"github.com/couchbase/indexing/secondary/common"
-	rand2 "math/rand"
+	"math/rand"
 	"runtime"
 	"sync"
 	"testing"
 	"time"
 )
 
-const keySize = 10
+const keySize = 25
+const snapIncrInterval = time.Millisecond * 10
+const snapInitInterval = time.Millisecond * 10
 
-func randString(r *rand2.Rand, n int) string {
+type ientry struct {
+	e     []byte
+	docid []byte
+	m     *MutationMeta
+}
+
+func randString(r *rand.Rand, n int) string {
 	const alphanum = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 	bytes := make([]byte, n)
 	for i := 0; i < n; i++ {
@@ -21,10 +29,10 @@ func randString(r *rand2.Rand, n int) string {
 	return string(bytes)
 }
 
-func mutationProducer(wg *sync.WaitGroup, s Slice, offset, n, id int, isRand bool) {
+func mutationProducer(wg *sync.WaitGroup, s Slice, offset, n, id int, isRand bool, stream chan *ientry) {
 	defer wg.Done()
 
-	rnd := rand2.New(rand2.NewSource(int64(rand2.Int())))
+	rnd := rand.New(rand.NewSource(int64(rand.Int())))
 	for i := 0; i < n; i++ {
 		docN := i + offset
 		if isRand {
@@ -33,23 +41,63 @@ func mutationProducer(wg *sync.WaitGroup, s Slice, offset, n, id int, isRand boo
 
 		docid := []byte(fmt.Sprintf("docid-%d", docN))
 		key := []byte("[\"" + randString(rnd, keySize) + "\"]")
-		//key := []byte(fmt.Sprintf("[\"%025d\"]", rnd.Int()))
-		k, err := GetIndexEntryBytesFromKey(key, docid, false)
+		entry, err := GetIndexEntryBytesFromKey(key, docid, false)
 		common.CrashOnError(err)
 
 		meta := NewMutationMeta()
 		meta.vbucket = Vbucket(id)
 
-		s.Insert(k, docid, meta)
-		meta.Free()
+		stream <- &ientry{e: entry, m: meta, docid: docid}
+
+	}
+}
+
+func flushWorker(wg *sync.WaitGroup, stream chan *ientry, n int, slice Slice) {
+	defer wg.Done()
+
+	for i := 0; i < n; i++ {
+		entry := <-stream
+		slice.Insert(entry.e, entry.docid, entry.m)
+		entry.m.Free()
+	}
+}
+
+func runFlusher(interval time.Duration, streams []chan *ientry, slice Slice, finch chan bool) {
+	var snap Snapshot
+	var wg sync.WaitGroup
+
+	for {
+		for _, ch := range streams {
+			n := len(ch)
+			wg.Add(1)
+			go flushWorker(&wg, ch, n, slice)
+		}
+
+		wg.Wait()
+
+		info, err := slice.NewSnapshot(nil, false)
+		common.CrashOnError(err)
+		if snap != nil {
+			snap.Close()
+		}
+		snap, err = slice.OpenSnapshot(info)
+		common.CrashOnError(err)
+
+		select {
+		case <-time.After(interval):
+		case <-finch:
+			return
+		}
 	}
 }
 
 func TestMemDBInsertionPerf(t *testing.T) {
 	var wg sync.WaitGroup
 
-	nPerWriter := 600000
+	finch := make(chan bool)
+	nPerWriter := 1000000
 	nw := runtime.GOMAXPROCS(0)
+	streams := make([]chan *ientry, nw)
 	stats := &IndexStats{}
 	stats.Init()
 	cfg := common.SystemConfig.SectionConfig("indexer.", true)
@@ -62,24 +110,35 @@ func TestMemDBInsertionPerf(t *testing.T) {
 
 	n := nw * nPerWriter
 
+	// Initial build
 	t1 := time.Now()
 	for i := 0; i < nw; i++ {
 		wg.Add(1)
-		go mutationProducer(&wg, slice, i*nPerWriter, nPerWriter, i, false)
+		streams[i] = make(chan *ientry, 500000)
+		go mutationProducer(&wg, slice, i*nPerWriter, nPerWriter, i, false, streams[i])
 	}
 
-	wg.Wait()
+	go func() {
+		wg.Wait()
+		finch <- true
+	}()
 
+	runFlusher(snapInitInterval, streams, slice, finch)
 	dur1 := time.Since(t1)
 
+	// Incremental update
 	t2 := time.Now()
 	for i := 0; i < nw; i++ {
 		wg.Add(1)
-		go mutationProducer(&wg, slice, i*nPerWriter, nPerWriter, i, true)
+		go mutationProducer(&wg, slice, i*nPerWriter, nPerWriter, i, false, streams[i])
 	}
 
-	wg.Wait()
+	go func() {
+		wg.Wait()
+		finch <- true
+	}()
 
+	runFlusher(snapIncrInterval, streams, slice, finch)
 	dur2 := time.Since(t2)
 	fmt.Printf("Initial build: %d items took %v -> %v items/s\n", n, dur1, float64(n)/dur1.Seconds())
 	fmt.Printf("Incr build: %d items took %v -> %v items/s\n", n, dur2, float64(n)/dur2.Seconds())
