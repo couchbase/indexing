bf37816338b92970e55914031ac09e9077527772
prataprc <prataprc@gmail.com>
2015-12-31 16:46:45 +0530

    tools/n1qlexpr: amended script to evaluate N1QL expressions.
    Change-Id: Icd4e82779f6205ea95d03a611810ab4122da0356

--- secondary/tools/n1qlexpr/n1ql_expr.go
+++ secondary/tools/n1qlexpr/n1ql_expr.go
@@ -2,20 +2,25 @@ package main
 
 import "log"
 import "flag"
+import "io/ioutil"
 import "fmt"
 import "encoding/json"
 
 import "github.com/couchbase/query/parser/n1ql"
-import "github.com/couchbase/query/expression"
+import qexpr "github.com/couchbase/query/expression"
+import qvalue "github.com/couchbase/query/value"
 
 var options struct {
 	expr string
+	data string
 	json bool
 }
 
 func argParse() {
 	flag.StringVar(&options.expr, "expr", "",
 		"input expression")
+	flag.StringVar(&options.data, "data", "",
+		"data similar to curl's --data")
 	flag.BoolVar(&options.json, "json", false,
 		"marshal parsed expression back to JSON")
 	flag.Parse()
@@ -27,6 +32,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	if options.json {
 		exprb, err := json.Marshal(expr)
 		if err != nil {
@@ -37,9 +43,23 @@ func main() {
 		fmt.Printf("expr: %T %v\n", expr, expr)
 		fmt.Printf("json: %T %v\n", exprstr, exprstr)
 	} else {
-		exprstr := expression.NewStringer().Visit(expr)
+		exprstr := qexpr.NewStringer().Visit(expr)
 		fmt.Printf("input: %v\n", options.expr)
 		fmt.Printf("expr: %T %v\n", expr, expr)
 		fmt.Printf("output: %T %v\n", exprstr, exprstr)
+	}
+
+	if expr != nil && options.data != "" {
+		if options.data[0] == '@' {
+			docbytes, err := ioutil.ReadFile(options.data[1:])
+			if err != nil {
+				log.Fatal(err)
+			}
+			context := qexpr.NewIndexContext()
+			doc := qvalue.NewAnnotatedValue(docbytes)
+			v, err := expr.Evaluate(doc, context)
+			fmsg := "Evaluate() scalar:%v err:%v"
+			fmt.Printf(fmsg, v, err)
+		}
 	}
 }
