c451a8dc2b9b00071104fd6541e5e6784ade8236
prataprc <prataprc@gmail.com>
2016-02-12 11:45:25 +0530

    MB100: tools/querycmd/ add benchmark for Timeit.
    Change-Id: Id585f34050a7f0da0029b037aa6be760ee52bb40

---
+++ secondary/tools/querycmd/benchtimeit.go
@@ -0,0 +1,15 @@
+package main
+
+import "time"
+import "fmt"
+
+import qclient "github.com/couchbase/indexing/secondary/queryport/client"
+
+func doBenchtimeit(cluster string, client *qclient.GsiClient) (err error) {
+	start := time.Now()
+	for i := 0; i < 1000000; i++ {
+		client.Bridge().Timeit(0x1111, 1)
+	}
+	fmt.Printf("time take by Timeit(): %v\n", time.Since(start)/1000000)
+	return
+}
