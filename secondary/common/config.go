f69209ec5d65087ed0562da308875ff70caef82a
Sriram Melkote <siri@couchbase.com>
2016-03-05 01:34:41 +0530

    Revert "MB-17799: Temporarily turn off backfill"
    This reverts commit 61b47f7343f84c1c68793cef5af9865e6b2f5f77.

--- secondary/common/config.go
+++ secondary/common/config.go
@@ -489,10 +489,10 @@ var SystemConfig = Config{
 		true, // immutable
 	},
 	"queryport.client.backfillLimit": ConfigValue{
-		0,
+		5 * 1024, // 5GB
 		"limit in mega-bytes to cap n1ql side backfilling, if ZERO backfill " +
 			"will be disabled.",
-		0,
+		5 * 1024, // 5GB
 		false,    // mutable
 	},
 	// projector's adminport client, can be used by indexer.
