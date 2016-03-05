daae30bc1744379865f2bdc72f6fdff5e4969fee
johliang00 <johliang00@yahoo.com>
2016-03-04 18:01:37 -0800

    MB-18540: spread scan request more evenly across nodes
    Change-Id: Id2189995fa8c13900f9cfde4f47506a29c11b840

--- secondary/common/config.go
+++ secondary/common/config.go
@@ -475,17 +475,17 @@ var SystemConfig = Config{
 		true, // immutable
 	},
 	"queryport.client.load.randomWeight": ConfigValue{
-		0.1,
+		0.9,
 		"random weightage between [0, 1.0) for random load-balancing, " +
 			"lower the value less likely for random load-balancing",
-		0.1,
+		0.9,
 		true, // immutable
 	},
 	"queryport.client.load.equivalenceFactor": ConfigValue{
-		0.9,
+		0.1,
 		"normalization factor on replica's avg-load to group them with " +
 			"least loaded replica.",
-		0.9,
+		0.1,
 		true, // immutable
 	},
 	"queryport.client.backfillLimit": ConfigValue{
