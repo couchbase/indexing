560c5d9da2154ac28dd906c50ed71a0ce74453c0
Sarath Lakshman <sarathlakshman@slynux.com>
2017-05-08 15:38:12 +0530

    MB-22948 Undo Revert "MB-100 indexer: Use readers-writer lock""
    This reverts commit 0f2e340af4f0cb83cb9d766b3954e47a810dba10.

--- secondary/indexer/stream_reader.go
+++ secondary/indexer/stream_reader.go
@@ -50,7 +50,7 @@ type mutationStreamReader struct {
 	stats IndexerStatsHolder
 
 	indexerState common.IndexerState
-	stateLock    sync.Mutex
+	stateLock    sync.RWMutex
 
 	queueMapLock sync.RWMutex
 	stopch       StopChannel
@@ -418,8 +418,8 @@ func (r *mutationStreamReader) logReaderStat() {
 }
 
 func (r *mutationStreamReader) getIndexerState() common.IndexerState {
-	r.stateLock.Lock()
-	defer r.stateLock.Unlock()
+	r.stateLock.RLock()
+	defer r.stateLock.RUnlock()
 	return r.indexerState
 }
 
