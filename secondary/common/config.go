65c98138e88e23320c49ef2f8a8143b3f9b73f66
Sarath Lakshman <sarathlakshman@slynux.com>
2017-11-30 09:54:22 +0530

    MB-26192 plasma: Disable fast recovery
    Change-Id: I2d9d371c0782bb451e6d0b1311f144a0bc7aea5b

--- secondary/common/config.go
+++ secondary/common/config.go
@@ -1205,7 +1205,7 @@ var SystemConfig = Config{
 	},
 
 	"indexer.plasma.checkpointInterval": ConfigValue{
-		600,
+		0, // Disable fast recovery
 		"Fast recovery checkpoint interval in seconds",
 		600,
 		false,
