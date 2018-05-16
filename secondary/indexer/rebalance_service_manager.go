5884d5792792e9d98014d63e45b011ebbfc1a481
deepkaran <deepkaran.salooja@gmail.com>
2018-05-16 15:53:07 -0700

    MB-29717 fix log message
    Change-Id: I4814ac44fefaa733a718e9a91b07fd9ce40a89b9

--- secondary/indexer/rebalance_service_manager.go
+++ secondary/indexer/rebalance_service_manager.go
@@ -2535,7 +2535,7 @@ func (m *ServiceMgr) getNodeIdFromDest(dest string) (string, error) {
 
 	}
 	errStr := fmt.Sprintf("Unable to find Index service for destination %v or desintation is not part of the cluster", dest)
-	l.Errorf("ServiceMgr::getNodeIdFromDest %v")
+	l.Errorf("ServiceMgr::getNodeIdFromDest %v", errStr)
 
 	return "", errors.New(errStr)
 }
