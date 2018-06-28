98961cf96674a95cd4e4c8748adbe6cb101bbfad
Sundar Sridharan <sundar.sridharan@gmail.com>
2018-06-27 22:24:06 -0700

    MB-26307 moi: set persistence_threads to 50% cpu capacity
    Change-Id: Id819839de54773764defa079fc907e9ba96cd09c

--- secondary/common/config.go
+++ secondary/common/config.go
@@ -1814,9 +1814,9 @@ var SystemConfig = Config{
 		false, // case-insensitive
 	},
 	"indexer.settings.moi.persistence_threads": ConfigValue{
-		(runtime.NumCPU()*3 + 1) / 4,
+		(runtime.GOMAXPROCS(0) + 1) / 2,
 		"Number of concurrent threads scanning index for persistence",
-		(runtime.NumCPU()*3 + 1) / 4,
+		(runtime.GOMAXPROCS(0) + 1) / 2,
 		false, // mutable
 		false, // case-insensitive
 	},
