0747eb54972ccdc2db06b94407ef0c4a70ecc2f7
Amit Kulkarni <amit.kulkarni@couchbase.com>
2020-01-24 15:51:26 +0530

    MB-31935: Introduce consumer based stat filter in /stats REST endpoint
    Various consumers of /stats, like planner, rebalancer can now request
    consumer specific stats. This will avoid a large sized payload from
    the response.
    
    Indexer's stats_manager will know which stats are needed by which
    consumer.
    
    Example:
    curl -X POST -u <username>:<password> http://<indexer-ip>:9102/stats?consumerFilter=planner
    
    Change-Id: I9f3c0af3ccb16e32a3b233139713880d6a9406c6

--- secondary/stats/timing.go
+++ secondary/stats/timing.go
@@ -7,12 +7,16 @@ type TimingStat struct {
 	Count   Int64Val
 	Sum     Int64Val
 	SumOfSq Int64Val
+	bitmap  uint64
 }
 
 func (t *TimingStat) Init() {
 	t.Count.Init()
 	t.Sum.Init()
 	t.SumOfSq.Init()
+
+	// Set the default value of filter bitmap to AllStatsFilter
+	t.bitmap = AllStatsFilter
 }
 
 func (t *TimingStat) Put(dur time.Duration) {
@@ -22,5 +26,17 @@ func (t *TimingStat) Put(dur time.Duration) {
 }
 
 func (t TimingStat) Value() string {
+	return fmt.Sprintf("%d %d %d", t.Count.Value(), t.Sum.Value(), t.SumOfSq.Value())
+}
+
+func (t *TimingStat) Map(bitmap uint64) bool {
+	return (t.bitmap & bitmap) != 0
+}
+
+func (t *TimingStat) AddFilter(filter uint64) {
+	t.bitmap |= filter
+}
+
+func (t *TimingStat) GetValue() interface{} {
 	return fmt.Sprintf("%d %d %d", t.Count.Value(), t.Sum.Value(), t.SumOfSq.Value())
 }
