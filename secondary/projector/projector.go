b120a8fc734978b10cd55a48b9c3e511a70eac42
Varun Velamuri <varun.velamuri@couchbase.com>
2020-06-04 11:50:39 +0530

    MB-40448 Do not accept cpu or memory profile names in settings endpoint
    BP of MB-39735 to alice
    
    The profile names will be defaulted to "indexer_cpu.pprof",
    "indexer_mem.pprof", "projector_cpu.pprof", "projector_mem.pprof"
    
    Additionally, the user can specify a directory where these files can
    go to using the options: "cpuProfDir", "memProfDir". Note that the
    directory should exist and has write permissions for couchbase user.
    Otherwise, the call would silently fail.
    
    Sample usage:
    curl -u <username>:<password> http://localhost:9102/settings
            -X POST -d '{"indexer.settings.cpuProfile":true,
                         "indexer.settings.cpuProfDir":"logs/"}'
    
    Change-Id: Ie17bae353c93776980825529b5d239b5f04db8e0
    (cherry picked from commit 3081e22d7d6889257a40d9cce75bc2d053cc0527)
    (cherry picked from commit 146d58511447aaa879280b2e87386ded893981a1)

--- secondary/projector/projector.go
+++ secondary/projector/projector.go
@@ -1,23 +1,26 @@
 package projector
 
-import "fmt"
-import "sync"
-import "io"
-import "time"
-import "os"
-import "net/http"
-import "strings"
-import "encoding/json"
-import "runtime"
-import "runtime/pprof"
-import "runtime/debug"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"runtime"
+	"runtime/debug"
+	"runtime/pprof"
+	"strings"
+	"sync"
+	"time"
 
-import ap "github.com/couchbase/indexing/secondary/adminport"
-import c "github.com/couchbase/indexing/secondary/common"
-import projC "github.com/couchbase/indexing/secondary/projector/client"
-import protobuf "github.com/couchbase/indexing/secondary/protobuf/projector"
-import "github.com/golang/protobuf/proto"
-import "github.com/couchbase/indexing/secondary/logging"
+	ap "github.com/couchbase/indexing/secondary/adminport"
+	c "github.com/couchbase/indexing/secondary/common"
+	"github.com/couchbase/indexing/secondary/logging"
+	projC "github.com/couchbase/indexing/secondary/projector/client"
+	protobuf "github.com/couchbase/indexing/secondary/protobuf/projector"
+	"github.com/golang/protobuf/proto"
+)
 
 // Projector data structure, a projector is connected to
 // one or more upstream kv-nodes. Works in tandem with
@@ -133,15 +136,13 @@ func (p *Projector) ResetConfig(config c.Config) {
 	// CPU-profiling
 	cpuProfile, ok := config["projector.cpuProfile"]
 	if ok && cpuProfile.Bool() && p.cpuProfFd == nil {
-		cpuProfFname, ok := config["projector.cpuProfFname"]
-		if ok {
-			fname := cpuProfFname.String()
-			logging.Infof("%v cpu profiling => %q\n", p.logPrefix, fname)
-			p.cpuProfFd = p.startCPUProfile(fname)
-
-		} else {
-			logging.Errorf("Missing cpu-profile o/p filename\n")
+		cpuProfDir, ok := config["projector.cpuProfDir"]
+		fname := "projector_cpu.pprof"
+		if ok && cpuProfDir.String() != "" {
+			fname = filepath.Join(cpuProfDir.String(), fname)
 		}
+		logging.Infof("%v cpu profiling => %q\n", p.logPrefix, fname)
+		p.cpuProfFd = p.startCPUProfile(fname)
 
 	} else if ok && !cpuProfile.Bool() {
 		if p.cpuProfFd != nil {
@@ -157,14 +158,13 @@ func (p *Projector) ResetConfig(config c.Config) {
 	// MEM-profiling
 	memProfile, ok := config["projector.memProfile"]
 	if ok && memProfile.Bool() {
-		memProfFname, ok := config["projector.memProfFname"]
-		if ok {
-			fname := memProfFname.String()
-			if p.takeMEMProfile(fname) {
-				logging.Infof("%v mem profile => %q\n", p.logPrefix, fname)
-			}
-		} else {
-			logging.Errorf("Missing mem-profile o/p filename\n")
+		memProfDir, ok := config["projector.memProfDir"]
+		fname := "projector_mem.pprof"
+		if ok && memProfDir.String() != "" {
+			fname = filepath.Join(memProfDir.String(), fname)
+		}
+		if p.takeMEMProfile(fname) {
+			logging.Infof("%v mem profile => %q\n", p.logPrefix, fname)
 		}
 	}
 
