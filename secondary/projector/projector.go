7c5244199f0a5386c8c8771ba3d076c9ecff484a
Sai Krishna Teja Kommaraju <sai.teja@couchbase.com>
2021-03-04 09:08:26 +0530

    MB-44746 : Fix Increase in initial index build time during rebalance by 9x
    BP to 6.6.2 of MB-44604
    
    While getting local VBuckets projector will need the latest cinfo
    updates while rebalancing hence should not use the optimized fetch for
    PoolChangeNotification.
    
    Change-Id: I16026c6bcaa6dc859e8dbd79a462bd739e1dae9c
    (cherry picked from commit ad614ff5415833944b087e2e6922a907ea610b66)

--- secondary/projector/projector.go
+++ secondary/projector/projector.go
@@ -94,6 +94,7 @@ func NewProjector(maxvbs int, config c.Config, certFile string, keyFile string) 
 	c.CrashOnError(err)
 	p.cinfoClient = cic
 	p.cinfoClient.SetUserAgent("projector")
+	p.cinfoClient.WatchRebalanceChanges()
 
 	p.stats = NewProjectorStats()
 	p.statsMgr = NewStatsManager(p.statsCmdCh, p.statsStopCh, config)
