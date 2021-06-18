f400eef6c89a0b6e2ffce2066d84d453bbfb0074
Sai Krishna Teja Kommaraju <sai.teja@couchbase.com>
2021-06-18 12:00:07 +0530

    MB-22722 : [BP for 7.0.1] Allow Plasma Index creation on Ephemeral Bucket
    Change-Id: Iec0e60a5b6888d8275f0b792a03b29357de402b3
    (cherry picked from commit 3b6e2cd7b9a499e7b7771b7ab5a8c5ad748e61ff)

--- secondary/indexer/indexer.go
+++ secondary/indexer/indexer.go
@@ -77,7 +77,7 @@ var (
 	ErrUnknownBucket            = errors.New("Unknown Bucket")
 	ErrIndexerNotActive         = errors.New("Indexer Not Active")
 	ErrInvalidMetadata          = errors.New("Invalid Metadata")
-	ErrBucketEphemeral          = errors.New("Ephemeral Buckets Must Use MOI Storage")
+	ErrBucketEphemeral          = errors.New("Ephemeral Buckets Must Use MOI or PLASMA Storage")
 )
 
 // Backup corrupt index data files
@@ -1636,8 +1636,10 @@ func (idx *indexer) handleCreateIndex(msg Message) {
 		}
 		return
 	}
-	if ephemeral && common.GetStorageMode() != common.MOI {
-		logging.Errorf("Indexer::handleCreateIndex \n\t Bucket %v is Ephemeral but GSI storage is not MOI")
+
+	storageMode := common.GetStorageMode()
+	if ephemeral && storageMode != common.MOI && storageMode != common.PLASMA {
+		logging.Errorf("Indexer::handleCreateIndex \n\t Bucket %v is Ephemeral but GSI storage is not MOI or PLASMA", indexInst.Defn.Bucket)
 		if clientCh != nil {
 			clientCh <- &MsgError{
 				err: Error{code: ERROR_BUCKET_EPHEMERAL,
@@ -1713,8 +1715,8 @@ func (idx *indexer) handleCreateIndex(msg Message) {
 	}
 
 	//allocate partition/slice
-	var partnInstMap PartitionInstMap
-	if partnInstMap, _, err = idx.initPartnInstance(indexInst, clientCh, false); err != nil {
+	partnInstMap, _, err := idx.initPartnInstance(indexInst, clientCh, false)
+	if err != nil {
 		for _, partnDefn := range partitions {
 			idx.stats.RemovePartitionStats(indexInst.InstId, partnDefn.GetPartitionId())
 		}
