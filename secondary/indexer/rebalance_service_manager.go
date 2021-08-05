c0fff37a63cb8c6f6e5d8f3f1cc7542c729c301c
Kevin Cherkauer <kevin.cherkauer@couchbase.com>
2021-08-05 14:15:08 -0700

    MB-46840 Fix progress reporting to ns_server during rebalance
    Backport to 7.0.1 from 7.1.0 of same MB.
    
    Change-Id: I26061a367b77ee5cf323c3873ce5b7ed0a761b1f

--- secondary/indexer/rebalance_service_manager.go
+++ secondary/indexer/rebalance_service_manager.go
@@ -85,6 +85,7 @@ type rebalanceContext struct {
 	rev    uint64
 }
 
+// incRev increments the rebalanceContext revision number and returns the OLD value.
 func (ctx *rebalanceContext) incRev() uint64 {
 	curr := ctx.rev
 	ctx.rev++
@@ -604,7 +605,7 @@ func (m *ServiceMgr) prepareRebalance(change service.TopologyChange) error {
 		}
 	}
 
-	m.setStateIsBalanced(true)
+	m.setStateIsBalanced(true) // kludge to disable UI "Rebalance" button during rebalance as ns_server doesn't
 	return nil
 }
 
@@ -836,6 +837,7 @@ func (m *ServiceMgr) checkExistingGlobalRToken() (*RebalanceToken, error) {
 
 }
 
+// initPreparePhaseRebalance is a helper for prepareRebalance.
 func (m *ServiceMgr) initPreparePhaseRebalance() error {
 
 	err := m.registerRebalanceRunning(true)
@@ -1236,6 +1238,7 @@ func (m *ServiceMgr) cleanupLocalRToken() error {
 	return nil
 }
 
+// monitorStartPhaseInit runs in a goroutine and checks whether rebalanceToken is created in a reasonable time.
 func (m *ServiceMgr) monitorStartPhaseInit(stopch StopChannel) error {
 
 	cfg := m.config.Load()
@@ -1255,7 +1258,7 @@ loop:
 				m.mu.Lock()
 				defer m.mu.Unlock()
 				if m.rebalanceToken == nil && elapsed > startPhaseBeginTimeout {
-					l.Infof("ServiceMgr::monitorStartPhaseInit Timout Waiting for RebalanceToken. Cleanup Prepare Phase")
+					l.Infof("ServiceMgr::monitorStartPhaseInit Timeout Waiting for RebalanceToken. Cleanup Prepare Phase")
 					//TODO handle server side differently
 					m.runCleanupPhaseLOCKED(RebalanceTokenPath, false)
 					done = true
