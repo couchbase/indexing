b95839be1f48402cff15a831826cdc4cd21e6fdc
Deepkaran <deepkaran@couchbase.com>
2021-10-30 13:27:45 -0700

    MB-45810 Add TestOSOInitBuildIndexerRestart
    Change-Id: I5936c2d3088bdbf1008576d2a1dd22e9086d590b

--- secondary/tests/functionaltests/set22_oso_test.go
+++ secondary/tests/functionaltests/set22_oso_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"testing"
+	"time"
 
 	c "github.com/couchbase/indexing/secondary/common"
 	tc "github.com/couchbase/indexing/secondary/tests/framework/common"
@@ -54,6 +55,76 @@ func TestOSOInitBuildDeleteMutation(t *testing.T) {
 	// Create a primary index
 	err = secondaryindex.CreateSecondaryIndex(indexName, bucketName, indexManagementAddress, "", nil, true, nil, true, defaultIndexActiveTimeout, nil)
 	FailTestIfError(err, "Error in creating the index", t)
+
+	scanResults, err := secondaryindex.ScanAll(indexName, bucketName, indexScanAddress, defaultlimit, c.SessionConsistency, nil)
+	FailTestIfError(err, "Error in scan", t)
+	if len(scanResults) != len(docs) {
+		log.Printf("Len of scanResults is incorrect. Expected and Actual are %d and %d", len(docs), len(scanResults))
+		err = errors.New("Len of scanResults is incorrect.")
+	}
+	FailTestIfError(err, "Len of scanResults is incorrect", t)
+
+	docScanResults := datautility.ExpectedScanResponse_RangePrimary(docs, "User2", "User5", 3)
+	scanResults, err = secondaryindex.Range(indexName, bucketName, indexScanAddress, []interface{}{"User2"}, []interface{}{"User5"}, 3, false, defaultlimit, c.SessionConsistency, nil)
+	FailTestIfError(err, "Error in scan", t)
+	err = tv.ValidateActual(docScanResults, scanResults)
+	FailTestIfError(err, "Error in scan result validation of Primary Range", t)
+
+	// Count Range on primary index
+	docScanResults = datautility.ExpectedScanResponse_RangePrimary(docs, "User2", "User5", 3)
+	rangeCount, err := secondaryindex.CountRange(indexName, bucketName, indexScanAddress, []interface{}{"User2"}, []interface{}{"User5"}, 3, c.SessionConsistency, nil)
+	FailTestIfError(err, "Error in CountRange: ", t)
+	log.Printf("CountRange() expected and actual is:  %d and %d", len(docScanResults), rangeCount)
+	if int64(len(docScanResults)) != rangeCount {
+		e := errors.New(fmt.Sprintf("Expected Range count %d does not match actual Range count %d: ", len(docScanResults), rangeCount))
+		FailTestIfError(e, "Error in CountRange: ", t)
+	}
+
+	var lookupkey string
+	for k := range docs {
+		lookupkey = k
+		break
+	}
+	log.Printf("lookupkey for CountLookup() = %v", lookupkey)
+
+	// Count Lookup on primary index
+	docScanResults = datautility.ExpectedScanResponse_RangePrimary(docs, lookupkey, lookupkey, 3)
+	lookupCount, err := secondaryindex.CountLookup(indexName, bucketName, indexScanAddress, []interface{}{lookupkey}, c.SessionConsistency, nil)
+	FailTestIfError(err, "Error in CountRange: ", t)
+	if int64(len(docScanResults)) != lookupCount {
+		e := errors.New(fmt.Sprintf("Expected Lookup count %d does not match actual Range count %d: ", len(docScanResults), lookupCount))
+		FailTestIfError(e, "Error in CountRange: ", t)
+	}
+	log.Printf("CountLookup() = %v", lookupCount)
+	secondaryindex.UseClient = tmp
+}
+
+func TestOSOInitBuildIndexerRestart(t *testing.T) {
+	log.Printf("In TestOSOInitBuildIndexerRestart()")
+	var indexName = "index_p2_oso"
+	var bucketName = "default"
+
+	tmp := secondaryindex.UseClient
+	secondaryindex.UseClient = "gsi"
+
+	// Create a primary index
+	err := secondaryindex.CreateSecondaryIndexAsync(indexName, bucketName, indexManagementAddress, "", nil, true, []byte("{\"defer_build\":   true}"), true, nil)
+	FailTestIfError(err, "Error in creating the index", t)
+
+	client, err := secondaryindex.GetOrCreateClient(indexManagementAddress, "test22client")
+	FailTestIfError(err, "Error in TestOSOInitBuildIndexerRestart while creating client", t)
+	defn1, _ := secondaryindex.GetDefnID(client, bucketName, indexName)
+
+	err = secondaryindex.BuildIndexesAsync([]uint64{defn1}, indexManagementAddress, defaultIndexActiveTimeout)
+	FailTestIfError(err, "Error from BuildIndexesAsync of index", t)
+	time.Sleep(100 * time.Millisecond)
+
+	tc.KillIndexer()
+	time.Sleep(5 * time.Second)
+
+	defnID, _ := secondaryindex.GetDefnID(client, bucketName, indexName)
+	err = secondaryindex.WaitTillIndexActive(defnID, client, defaultIndexActiveTimeout)
+	FailTestIfError(err, "Error in WaitTillIndexActive", t)
 
 	scanResults, err := secondaryindex.ScanAll(indexName, bucketName, indexScanAddress, defaultlimit, c.SessionConsistency, nil)
 	FailTestIfError(err, "Error in scan", t)
