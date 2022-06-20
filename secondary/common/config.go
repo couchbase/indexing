2493caf6c13844757e35f1766413979aa00bf747
Deepkaran Salooja <deepkaran@couchbase.com>
2022-06-20 11:28:39 -0700

    MB-52109 New config for serverless low/high memory threshold
    Change-Id: I85a495c0586c389c71d811c2b2114cbb2eaa1f54

--- secondary/common/config.go
+++ secondary/common/config.go
@@ -3263,6 +3263,26 @@ var SystemConfig = Config{
 		false, // mutable
 		false, // case-insensitive
 	},
+
+	"indexer.settings.thresholds.mem_high": ConfigValue{
+		80,
+		"Percentage of memory_quota usage above which Indexer node " +
+			"is considered to have maxed out the memory usage and needs " +
+			"additional capacity.",
+		80,
+		false, // mutable
+		false, // case-insensitive
+	},
+
+	"indexer.settings.thresholds.mem_low": ConfigValue{
+		60,
+		"Percentage of memory_quota usage above which Indexer node " +
+			"doesn't accept new create index except for existing tenants. " +
+			"The capacity between low and high threshold is for existing tenant growth.",
+		60,
+		false, // mutable
+		false, // case-insensitive
+	},
 }
 
 // NewConfig from another
