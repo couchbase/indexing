c7a7eebf55d079d21f4db8d5a8bec148b9b0cc28
akhilmd <akhil.m.durga2@gmail.com>
2022-07-20 16:40:09 +0530

    MB-55312 [BP]: Log nil values in NodeTable
    Change-Id: I601c928a9be0793fa0e22f8ea0b36fc6aea498f2

--- secondary/memdb/nodetable/table.go
+++ secondary/memdb/nodetable/table.go
@@ -16,6 +16,7 @@ import (
 	"fmt"
 	"unsafe"
 
+	"github.com/couchbase/indexing/secondary/logging"
 	"github.com/couchbase/indexing/secondary/memdb/skiplist"
 )
 
@@ -116,6 +117,17 @@ func (nt *NodeTable) Get(key []byte) unsafe.Pointer {
 	return nil
 }
 
+func hasNil(slowVals []uint64) bool {
+	for _, val := range slowVals {
+		// slowVals conflict bit is unmarked, so no need to decode
+		if val <= 0 {
+			return true
+		}
+	}
+
+	return false
+}
+
 func (nt *NodeTable) Update(key []byte, nptr unsafe.Pointer) (updated bool, oldPtr unsafe.Pointer) {
 	res := nt.find(key)
 	if res.status&ntFoundMask == ntFoundMask {
@@ -123,9 +135,17 @@ func (nt *NodeTable) Update(key []byte, nptr unsafe.Pointer) (updated bool, oldP
 		updated = true
 		if res.status == ntFoundInFast {
 			oldPtr = decodePointer(res.fastHTValue)
+			if nptr == nil {
+				msg := fmt.Sprintf("NodeTable::Update: Found in fast: oldPtr[%p] nptr[%p] res.hasConflict[%v]", oldPtr, nptr, res.hasConflict)
+				logging.Fatalf(msg)
+			}
 			nt.fastHT[res.hash] = encodePointer(nptr, res.hasConflict)
 		} else {
 			oldPtr = decodePointer(res.slowHTValues[res.slowHTPos])
+			if nptr == nil {
+				msg := fmt.Sprintf("NodeTable::Update: Found in slow: oldPtr[%p] nptr[%p] res.hasConflict[%v] res.slowHTPos[%d] res.slowHTValues[%v]", oldPtr, nptr, res.hasConflict, res.slowHTPos, res.slowHTValues)
+				logging.Fatalf(msg)
+			}
 			res.slowHTValues[res.slowHTPos] = encodePointer(nptr, true)
 		}
 	} else {
@@ -135,17 +155,31 @@ func (nt *NodeTable) Update(key []byte, nptr unsafe.Pointer) (updated bool, oldP
 		// Key needs to be inserted into slowHT
 		if res.hasConflict || newSlowValue {
 			slowHTValues := nt.slowHT[res.hash]
+			oldSlowHTValues := append([]uint64{}, slowHTValues...)
 			slowHTValues = append(slowHTValues, encodePointer(nptr, false))
+			if hasNil(slowHTValues) {
+				msg := fmt.Sprintf("NodeTable::Update: Not found, has collision: slowHTValues[%v] oldSlowHTValues[%v]", slowHTValues, oldSlowHTValues)
+				logging.Fatalf(msg)
+			}
 			nt.slowHT[res.hash] = slowHTValues
 			// There is an entry already in the fastHT for same crc32 hash
 			// We have inserted first entry into the slowHT. Now mark conflict bit.
 			if newSlowValue {
-				nt.fastHT[res.hash] = encodePointer(decodePointer(nt.fastHT[res.hash]), true)
+				oldFastPtr := decodePointer(nt.fastHT[res.hash])
+				if oldFastPtr == nil {
+					msg := fmt.Sprintf("NodeTable::Update: Not found, first collision, added to slowHT, marking fastHT: oldFastPtr[%p] res.hasConflict[%v] res.fastHTHasEntry[%v]", oldFastPtr, res.hasConflict, res.fastHTHasEntry)
+					logging.Fatalf(msg)
+				}
+				nt.fastHT[res.hash] = encodePointer(oldFastPtr, true)
 				nt.conflicts++
 			}
 			nt.slowHTCount++
 		} else {
 			// Insert new item into fastHT
+			if nptr == nil {
+				msg := fmt.Sprintf("NodeTable::Update: Not found, no collision, adding to fastHT: nptr[%p] res.hasConflict[%v] res.fastHTHasEntry[%v]", nptr, res.hasConflict, res.fastHTHasEntry)
+				logging.Fatalf(msg)
+			}
 			nt.fastHT[res.hash] = encodePointer(nptr, false)
 			nt.fastHTCount++
 		}
@@ -164,6 +198,7 @@ func (nt *NodeTable) Remove(key []byte) (success bool, nptr unsafe.Pointer) {
 			// an item present in slowHT and overwrite fastHT entry.
 			if res.hasConflict {
 				slowHTValues := nt.slowHT[res.hash]
+				oldSlowHTValues := append([]uint64{}, slowHTValues...)
 				v := slowHTValues[0] // New fastHT candidate
 				slowHTValues = append([]uint64(nil), slowHTValues[1:]...)
 				nt.slowHTCount--
@@ -174,16 +209,26 @@ func (nt *NodeTable) Remove(key []byte) (success bool, nptr unsafe.Pointer) {
 					nt.conflicts--
 				} else {
 					conflict = true
+					if hasNil(slowHTValues) {
+						msg := fmt.Sprintf("NodeTable::Remove: shift slow[0] out: slowHTValues[%v] oldSlowHTValues[%v]", slowHTValues, oldSlowHTValues)
+						logging.Fatalf(msg)
+					}
 					nt.slowHT[res.hash] = slowHTValues
 				}
 
-				nt.fastHT[res.hash] = encodePointer(decodePointer(v), conflict)
+				newFastPtr := decodePointer(v)
+				if newFastPtr == nil {
+					msg := fmt.Sprintf("NodeTable::Remove: add slow[0] to fast: newFastPtr[%p] conflict[%v] nptr[%p] oldSlowHTValues[%v] slowHTValues[%v]", newFastPtr, conflict, nptr, oldSlowHTValues, slowHTValues)
+					logging.Fatalf(msg)
+				}
+				nt.fastHT[res.hash] = encodePointer(newFastPtr, conflict)
 			} else {
 				delete(nt.fastHT, res.hash)
 				nt.fastHTCount--
 			}
 		} else {
 			nptr = decodePointer(res.slowHTValues[res.slowHTPos])
+			oldSlowHTValues := append([]uint64{}, res.slowHTValues...)
 			// Remove key from slowHT
 			newSlowValue := append([]uint64(nil), res.slowHTValues[:res.slowHTPos]...)
 			if res.slowHTPos+1 != len(res.slowHTValues) {
@@ -193,9 +238,18 @@ func (nt *NodeTable) Remove(key []byte) (success bool, nptr unsafe.Pointer) {
 
 			if len(newSlowValue) == 0 {
 				delete(nt.slowHT, res.hash)
-				nt.fastHT[res.hash] = encodePointer(decodePointer(nt.fastHT[res.hash]), false)
+				fastPtr := decodePointer(nt.fastHT[res.hash])
+				if fastPtr == nil {
+					msg := fmt.Sprintf("NodeTable::Remove: no more conflicts, remark fast: fastPtr[%p] nptr[%p] oldSlowHTValues[%v] newSlowValue[%v] res.slowHTPos[%d]", fastPtr, nptr, oldSlowHTValues, newSlowValue, res.slowHTPos)
+					logging.Fatalf(msg)
+				}
+				nt.fastHT[res.hash] = encodePointer(fastPtr, false)
 				nt.conflicts--
 			} else {
+				if hasNil(newSlowValue) {
+					msg := fmt.Sprintf("NodeTable::Remove: adjusted slow: nptr[%p] oldSlowHTValues[%v] newSlowValue[%v] res.slowHTPos[%d]", nptr, oldSlowHTValues, newSlowValue, res.slowHTPos)
+					logging.Fatalf(msg)
+				}
 				nt.slowHT[res.hash] = newSlowValue
 			}
 		}
