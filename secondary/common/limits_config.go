8c0a4cdc8b4220f65e90e78b090c237d5c44545c
Amit Kulkarni <amit.kulkarni@couchbase.com>
2023-04-19 13:30:22 +0530

    MB-56328: Remove free tier support from indexing
    To avoid removal of code from multiple different components, the
    limit enforcement is skipped in the code.
    
    Change-Id: Id63c135d04adb6f18bb00d5ba62c24bd5a9c2d0a

--- secondary/common/limits_config.go
+++ secondary/common/limits_config.go
@@ -8,12 +8,6 @@
 
 package common
 
-import (
-	"github.com/couchbase/cbauth"
-	"github.com/couchbase/indexing/secondary/logging"
-	"github.com/couchbase/indexing/secondary/security"
-)
-
 type LimitsCache struct{}
 
 func NewLimitsCache() (*LimitsCache, error) {
@@ -31,20 +25,10 @@ func NewLimitsCache() (*LimitsCache, error) {
 // call and returns last cached value received from ns_server.
 //
 
-//
 // This should return true only when limits are enforced
 // and cluster version >= 7.1
 //
+// Update: Remove free tier limits as per MB-56328
 func (lcCache *LimitsCache) EnforceLimits() (bool, error) {
-	if security.IsToolsConfigUsed() {
-		return false, nil
-	}
-	lc, err := cbauth.GetLimitsConfig()
-
-	if err != nil {
-		logging.Errorf("Error in EnforceLimits. error %v", err)
-		return false, err
-	}
-
-	return lc.EnforceLimits, nil
+	return false, nil
 }
