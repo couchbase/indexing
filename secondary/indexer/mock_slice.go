dfecc667390f7f33648ee9458d0ef5b57b312de8
SAI KRISHNA TEJA KOMMARAJU <sai.teja@couchbase.com>
2024-06-11 00:36:22 +0530

    MB-62251: Add unit tests for testing ScanWorker
    * Add mock_slice to test the ScanWorker
    * MockSnapshot will get a function run in parallel and get the data
      needed to send via iterator
    * The feeded function will be provided by test
    * Add varaibles in MockCodebook to inject error
    
    Change-Id: Id63ef558c750122707c05f536ef9e24e4c7e4be6

---
+++ secondary/indexer/mock_slice.go
@@ -0,0 +1,432 @@
+package indexer
+
+import (
+	"time"
+
+	c "github.com/couchbase/indexing/secondary/common"
+	"github.com/couchbase/indexing/secondary/logging"
+)
+
+type snapshotFeeder func(datach chan Row, errch chan error)
+
+// -----
+// Slice
+// -----
+
+type mockSlice struct {
+	ID          SliceId
+	PathStr     string
+	SliceStatus SliceStatus
+
+	IdxDefn c.IndexDefn
+
+	InstID   c.IndexInstId
+	PartnID  c.PartitionId
+	DefnID   c.IndexDefnId
+	ShardIDs []c.ShardId
+
+	IsActiveFlag        bool
+	IsDirtyFlag         bool
+	CleanupDoneFlag     bool
+	RecoveryDoneFlag    bool
+	BuildDoneFlag       bool
+	RebalRunningFlag    bool
+	PersistorActiveFlag bool
+	FlushDoneFlag       bool
+
+	WriteUnits    uint64
+	StopWUBilling bool
+
+	cfg c.Config
+
+	snap    Snapshot
+	testErr error
+	ts      *c.TsVbuuid
+}
+
+func (ms *mockSlice) Id() SliceId {
+	return ms.ID
+}
+
+func (ms *mockSlice) Path() string {
+	ms.PathStr = "/tmp/mockslice/"
+	return ms.PathStr
+}
+
+func (ms *mockSlice) Status() SliceStatus {
+	ms.SliceStatus = SLICE_STATUS_ACTIVE
+	return ms.SliceStatus
+}
+
+func (ms *mockSlice) IndexInstId() c.IndexInstId {
+	return ms.InstID
+}
+func (ms *mockSlice) IndexPartnId() c.PartitionId {
+	return ms.PartnID
+}
+
+func (ms *mockSlice) IndexDefnId() c.IndexDefnId {
+	return ms.DefnID
+}
+
+func (ms *mockSlice) IsActive() bool {
+	ms.IsActiveFlag = true
+	return ms.IsActiveFlag
+}
+
+func (ms *mockSlice) IsDirty() bool {
+	return ms.IsDirtyFlag
+}
+
+func (ms *mockSlice) IsCleanupDone() bool {
+	return ms.CleanupDoneFlag
+}
+
+func (ms *mockSlice) SetActive(active bool) {
+	ms.IsActiveFlag = active
+}
+
+func (ms *mockSlice) SetStatus(ss SliceStatus) {
+	ms.SliceStatus = ss
+}
+
+func (ms *mockSlice) UpdateConfig(cfg c.Config) {
+	ms.cfg = cfg
+}
+
+func (ms *mockSlice) RecoveryDone() {
+	ms.RecoveryDoneFlag = true
+}
+
+func (ms *mockSlice) BuildDone() {
+	ms.BuildDoneFlag = true
+}
+
+func (ms *mockSlice) ClearRebalRunning() {
+	ms.RebalRunningFlag = false
+}
+
+func (ms *mockSlice) SetRebalRunning() {
+	ms.RebalRunningFlag = true
+}
+
+func (ms *mockSlice) IsPersistanceActive() bool {
+	return ms.PersistorActiveFlag
+}
+
+func (ms *mockSlice) GetWriteUnits() uint64 {
+	return ms.WriteUnits
+}
+
+func (ms *mockSlice) SetStopWriteUnitBilling(disableBilling bool) {
+	ms.StopWUBilling = disableBilling
+}
+
+func (ms *mockSlice) Insert(key []byte, docid []byte, meta *MutationMeta) error {
+	if ms.testErr != nil {
+		return ms.testErr
+	}
+	return nil
+}
+
+func (ms *mockSlice) Delete(docid []byte, meta *MutationMeta) error {
+	if ms.testErr != nil {
+		return ms.testErr
+	}
+	return nil
+}
+
+func (ms *mockSlice) GetShardIds() []c.ShardId {
+	return ms.ShardIDs
+}
+
+func (ms *mockSlice) GetAlternateShardId(c.PartitionId) string {
+	return ""
+}
+
+func (ms *mockSlice) FlushDone() {
+	ms.FlushDoneFlag = true
+}
+
+func (ms *mockSlice) Rollback(s SnapshotInfo) error {
+	if ms.testErr != nil {
+		return ms.testErr
+	}
+
+	return nil
+}
+
+func (ms *mockSlice) RollbackToZero(bool) error {
+	if ms.testErr != nil {
+		return ms.testErr
+	}
+
+	return nil
+}
+
+func (ms *mockSlice) LastRollbackTs() *c.TsVbuuid {
+	return nil
+}
+
+func (ms *mockSlice) SetLastRollbackTs(ts *c.TsVbuuid) {
+}
+
+func (ms *mockSlice) Statistics(consumerFilter uint64) (StorageStatistics, error) {
+	return StorageStatistics{}, nil
+}
+
+func (ms *mockSlice) PrepareStats() {
+}
+
+func (ms *mockSlice) ShardStatistics(c.PartitionId) *c.ShardStats {
+	return nil
+}
+
+func (ms *mockSlice) GetTenantDiskSize() (int64, error) {
+	return 0, nil
+}
+
+func (ms *mockSlice) IncrRef() {
+}
+
+func (ms *mockSlice) DecrRef() {
+}
+
+func (ms *mockSlice) CheckAndIncrRef() bool {
+	return true
+}
+
+func (ms *mockSlice) Compact(abortTime time.Time, minFrag int) error {
+	return nil
+}
+
+func (ms *mockSlice) Close() {
+}
+
+func (ms *mockSlice) Destroy() {
+}
+
+func (ms *mockSlice) GetReaderContext(user string, skipReadMetering bool) IndexReaderContext {
+	return nil
+}
+
+func (ms *mockSlice) NewSnapshot(*c.TsVbuuid, bool) (SnapshotInfo, error) {
+	return nil, nil
+}
+
+func (ms *mockSlice) OpenSnapshot(SnapshotInfo) (Snapshot, error) {
+	if ms.testErr != nil {
+		return nil, ms.testErr
+	}
+
+	m := &mockSnapshot{
+		slice: ms,
+	}
+	return m, nil
+}
+
+func (ms *mockSlice) GetSnapshots() ([]SnapshotInfo, error) {
+	if ms.testErr != nil {
+		return nil, ms.testErr
+	}
+
+	return nil, nil
+}
+
+func (ms *mockSlice) SetNlist(int)          {}
+func (ms *mockSlice) InitCodebook() error   { return nil }
+func (ms *mockSlice) ResetCodebook() error  { return nil }
+func (ms *mockSlice) Train([]float32) error { return nil }
+
+// -------------
+// SliceSnapshot
+// -------------
+
+type mockSliceSnapshot struct {
+	mockSlice *mockSlice
+	snap      *mockSnapshot
+}
+
+func (mss *mockSliceSnapshot) SliceId() SliceId {
+	return mss.mockSlice.ID
+}
+
+func (mss *mockSliceSnapshot) Snapshot() Snapshot {
+	if mss.snap != nil {
+		return mss.snap
+	}
+	return nil
+}
+
+// ------------------
+// IndexReaderContext
+// ------------------
+type mockReaderContext struct {
+}
+
+func (mrc *mockReaderContext) Init(chan bool) bool            { return true }
+func (mrc *mockReaderContext) Done()                          {}
+func (mrc *mockReaderContext) SetCursorKey(cur *[]byte)       {}
+func (mrc *mockReaderContext) GetCursorKey() *[]byte          { return nil }
+func (mrc *mockReaderContext) User() string                   { return "" }
+func (mrc *mockReaderContext) ReadUnits() uint64              { return 0 }
+func (mrc *mockReaderContext) RecordReadUnits(byteLen uint64) {}
+func (mrc *mockReaderContext) SkipReadMetering() bool         { return true }
+
+// ------------
+// SnapshotInfo
+// ------------
+type mockSnapshotInfo struct {
+}
+
+func (msi *mockSnapshotInfo) Timestamp() *c.TsVbuuid        { return nil }
+func (msi *mockSnapshotInfo) IsCommitted() bool             { return true }
+func (msi *mockSnapshotInfo) IsOSOSnap() bool               { return false }
+func (msi *mockSnapshotInfo) Stats() map[string]interface{} { return nil }
+
+// --------
+// Snapshot
+// --------
+type mockSnapshot struct {
+	slice       *mockSlice
+	snapVersion int
+
+	id        SliceId
+	indInstid c.IndexInstId
+	indDefId  c.IndexDefnId
+	ts        *c.TsVbuuid
+
+	count  uint64
+	exists bool
+	err    error
+
+	datach chan Row
+	errch  chan error
+	order  SortOrder
+
+	feeder snapshotFeeder
+}
+
+func (ms *mockSnapshot) Open() error                { return nil }
+func (ms *mockSnapshot) Close() error               { return nil }
+func (ms *mockSnapshot) IsOpen() bool               { return true }
+func (ms *mockSnapshot) Id() SliceId                { return ms.id }
+func (ms *mockSnapshot) IndexInstId() c.IndexInstId { return ms.indInstid }
+func (ms *mockSnapshot) IndexDefnId() c.IndexDefnId { return ms.indDefId }
+func (ms *mockSnapshot) Timestamp() *c.TsVbuuid     { return ms.ts }
+func (ms *mockSnapshot) Info() SnapshotInfo         { return &mockSnapshotInfo{} }
+
+// -----------
+// IndexReader
+// -----------
+
+func (ms *mockSnapshot) Range(ctx IndexReaderContext, start IndexKey, end IndexKey, incl Inclusion,
+	cb EntryCallback) error {
+
+	if ms.feeder != nil {
+		ms.datach = make(chan Row)
+		ms.errch = make(chan error)
+		go ms.feeder(ms.datach, ms.errch)
+		for {
+			select {
+			case err, _ := <-ms.errch:
+				logging.Infof("[mockSnapshot::Range] got error from feeder: %v", err)
+				return err
+			case d, ok := <-ms.datach:
+				if !ok {
+					return nil
+				}
+				err := cb(d.key, d.value)
+				if err != nil {
+					logging.Infof("[mockSnapshot::Range] got error from callback: %v", err)
+					return err
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func (ms *mockSnapshot) CountTotal(ctx IndexReaderContext, stopch StopChannel) (uint64, error) {
+	return ms.count, ms.err
+}
+func (ms *mockSnapshot) StatCountTotal() (uint64, error) { return 0, nil }
+func (ms *mockSnapshot) Exists(ctx IndexReaderContext, Indexkey IndexKey, stopch StopChannel) (bool, error) {
+	return ms.exists, ms.err
+}
+func (ms *mockSnapshot) Lookup(IndexReaderContext, IndexKey, EntryCallback) error { return nil }
+
+func (ms *mockSnapshot) CountRange(ctx IndexReaderContext, low, high IndexKey, inclusion Inclusion,
+	stopch StopChannel) (uint64, error) {
+	ms.datach = make(chan Row)
+	ms.errch = make(chan error)
+	ms.count = 0
+	go ms.feeder(ms.datach, ms.errch)
+
+loop:
+	for {
+		select {
+		case ms.err, _ = <-ms.errch:
+			break loop
+		case _, ok := <-ms.datach:
+			if !ok {
+				break loop
+			}
+			ms.count++
+		}
+	}
+
+	return ms.count, ms.err
+}
+
+func (ms *mockSnapshot) CountLookup(ctx IndexReaderContext, keys []IndexKey, stopch StopChannel) (uint64, error) {
+	return 0, nil
+}
+
+func (ms *mockSnapshot) MultiScanCount(ctx IndexReaderContext, low, high IndexKey, inclusion Inclusion, scan Scan,
+	distinct bool, stopch StopChannel) (uint64, error) {
+	return 0, nil
+}
+
+func (ms *mockSnapshot) All(IndexReaderContext, EntryCallback) error { return nil }
+
+func (ms *mockSnapshot) FindNearestCentroids(vec []float32, k int64) ([]int64, error) {
+	return nil, nil
+}
+
+func (ms *mockSnapshot) ComputeDistanceTable(vec []float32) ([][]float32, error) {
+	return nil, nil
+}
+
+func (ms *mockSnapshot) ComputeDistanceWithDT(code []byte, dtable [][]float32) float32 {
+	return 0
+}
+
+func (ms *mockSnapshot) ComputeDistance(qvec []float32, fvecs []float32, dist []float32) error {
+	return nil
+}
+
+func (ms *mockSnapshot) DecodeVector(code []byte, vec []float32) error {
+	return nil
+}
+
+func (ms *mockSnapshot) DecodeVectors(n int, codes []byte, vecs []float32) error {
+	return nil
+}
+
+// ----------------------
+// Other Setter functions
+// ----------------------
+
+func (s *mockSlice) SetTimestamp(ts *c.TsVbuuid) error {
+	s.ts = ts
+	return nil
+}
+
+func (s *mockSlice) Timestamp() *c.TsVbuuid {
+	return s.ts
+}
+
+func (s *mockSnapshot) SetTimestamp(ts *c.TsVbuuid) {
+	s.ts = ts
+}
---
+++ secondary/indexer/scan_pipeline_vector_test.go
@@ -0,0 +1,280 @@
+package indexer
+
+import (
+	"fmt"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/couchbase/indexing/secondary/collatejson"
+	"github.com/couchbase/indexing/secondary/common"
+	c "github.com/couchbase/indexing/secondary/common"
+	json "github.com/couchbase/indexing/secondary/common/json"
+	"github.com/couchbase/indexing/secondary/logging"
+	protobuf "github.com/couchbase/indexing/secondary/protobuf/query"
+	"github.com/couchbase/indexing/secondary/vector/codebook"
+	n1ql "github.com/couchbase/query/value"
+	"github.com/golang/protobuf/proto"
+)
+
+// ----
+// name - (a to e)
+// age - (1 - 9)
+// color - (Red AnotherRed Blue AnotherBlue Green AnotherGreen)
+// color vectors - Red -> [(1.0, 0.0, 0.0) (0.8, 0.0, 0.0), (0.0, 1.0, 0.0), (0.0, 0.8, 0.0), (0.0, 0.0, 1.0), (0.0, 0.0, 0.8)]
+// -----
+var gCount uint64
+
+func getVectorDataFeeder(feedError bool, errDocId int, inputErr error,
+	injectDelay bool, delayDocId int, delay time.Duration, addHex bool) snapshotFeeder {
+
+	return func(datach chan Row, errch chan error) {
+		var colors = []struct {
+			id  string
+			v   []float64
+			vv  []int
+			hex string
+		}{
+			{"1", []float64{0.4, 1.0, 0.4}, []int{102, 255, 102}, "#66ff66"},
+			{"1", []float64{0.5, 1.0, 0.4}, []int{127, 255, 102}, "#7fff66"},
+			{"1", []float64{0.5, 1.0, 0.5}, []int{127, 255, 127}, "#7fff7f"},
+			{"1", []float64{0.4, 1.0, 0.5}, []int{102, 255, 127}, "#66ff7f"},
+			{"2", []float64{0.4, 0.5, 1.0}, []int{102, 127, 255}, "#667fff"},
+			{"2", []float64{0.5, 0.5, 1.0}, []int{127, 127, 255}, "#7f7fff"},
+			{"2", []float64{0.5, 0.4, 1.0}, []int{127, 102, 255}, "#7f66ff"},
+			{"2", []float64{0.4, 0.4, 1.0}, []int{102, 102, 255}, "#6666ff"},
+			{"3", []float64{1.0, 0.4, 0.4}, []int{255, 102, 102}, "#ff6666"},
+			{"3", []float64{1.0, 0.5, 0.4}, []int{255, 127, 102}, "#ff7f66"},
+			{"3", []float64{1.0, 0.5, 0.5}, []int{255, 127, 127}, "#ff7f7f"},
+			{"3", []float64{1.0, 0.4, 0.5}, []int{255, 102, 127}, "#ff667f"},
+			{"4", []float64{0.02, 0.6, 0.76}, []int{5, 153, 193}, "#599c1"},
+			{"4", []float64{0.02, 0.6, 0.76}, []int{5, 153, 193}, "#599c1"},
+			{"4", []float64{0.02, 0.6, 0.76}, []int{5, 153, 193}, "#599c1"},
+		}
+
+		docid := 0
+	toploop:
+		for _, name := range []string{"a", "a", "b", "b", "c", "c", "d", "d", "e", "e"} {
+			for age := 30; age < 40; age++ {
+				for _, c := range colors {
+					codec := collatejson.NewCodec(16)
+
+					keyArray := make([]interface{}, 3)
+					keyArray[0] = name
+					keyArray[1] = age
+					keyArray[2] = c.id
+					if addHex {
+						keyArray = append(keyArray, c.hex)
+					}
+
+					keyn1ql := n1ql.NewValue(keyArray)
+					buf := make([]byte, 0, 1000)
+					keyEntry, err := codec.EncodeN1QLValue(keyn1ql, buf)
+					if err != nil {
+						errch <- err
+						break toploop
+					}
+
+					valArray := make([]interface{}, len(c.v))
+					for i, v := range c.v {
+						valArray[i] = v
+					}
+					valn1ql := n1ql.NewValue(valArray)
+					buf = make([]byte, 0, 1000)
+					valEntry, err := codec.EncodeN1QLValue(valn1ql, buf)
+					if err != nil {
+						errch <- err
+						break toploop
+					}
+
+					cfg := common.SystemConfig.SectionConfig("indexer.", true)
+					szCfg := getKeySizeConfig(cfg)
+					g := atomic.LoadUint64(&gCount)
+					docidByte := []byte(fmt.Sprintf("docid-%d", g))
+					buf = make([]byte, 0, 1000)
+					secIdxEntry, err := NewSecondaryIndexEntry2(keyEntry, docidByte, false, 0, nil, buf, false, nil, szCfg)
+					if err != nil {
+						errch <- err
+						break toploop
+					}
+
+					datach <- Row{
+						key:   secIdxEntry,
+						value: valEntry,
+					}
+					atomic.AddUint64(&gCount, 1)
+
+					docid++
+					if feedError && docid == errDocId {
+						errch <- inputErr
+						break toploop
+					}
+					if injectDelay && docid == delayDocId {
+						time.Sleep(delay)
+					}
+				}
+			}
+		}
+		close(datach)
+	}
+}
+
+func getScanRequest1(dim, vectorKeyPos int, queryVector []float32) *ScanRequest {
+	sr := &ScanRequest{
+		isPrimary:   false,
+		vectorPos:   vectorKeyPos,
+		queryVector: queryVector,
+		IndexInst: c.IndexInst{
+			Defn: c.IndexDefn{
+				VectorMeta: &c.VectorMetadata{
+					Dimension:  dim,
+					Similarity: common.L2,
+				},
+				SecExprs: []string{"name", "age", "color"},
+			},
+		},
+		keySzCfg: keySizeConfig{
+			allowLargeKeys: true,
+			maxSecKeyLen:   4608,
+		},
+	}
+	return sr
+}
+
+func getSliceSnapshot1(feeder snapshotFeeder) SliceSnapshot {
+	sr := &mockSnapshot{feeder: feeder}
+	ss := &mockSliceSnapshot{snap: sr}
+	return ss
+}
+
+func getProtoScans(centroidId string) []*protobuf.Scan {
+	protoScans := make([]*protobuf.Scan, 0)
+	f := make([]*protobuf.CompositeElementFilter, 3)
+	ll, _ := json.Marshal("a")
+	hh, _ := json.Marshal("c")
+	f[0] = &protobuf.CompositeElementFilter{
+		Low:       ll,
+		High:      hh,
+		Inclusion: proto.Uint32(uint32(3)),
+	}
+	ll, _ = json.Marshal(34)
+	hh, _ = json.Marshal(39)
+	f[1] = &protobuf.CompositeElementFilter{
+		Low:       ll,
+		High:      hh,
+		Inclusion: proto.Uint32(uint32(3)),
+	}
+	ll, _ = json.Marshal(centroidId)
+	hh, _ = json.Marshal(centroidId)
+	f[2] = &protobuf.CompositeElementFilter{
+		Low:       ll,
+		High:      hh,
+		Inclusion: proto.Uint32(uint32(3)),
+	}
+	s := &protobuf.Scan{Filters: f}
+	protoScans = append(protoScans, s)
+	return protoScans
+}
+
+func TestVectorPipelineScanWorker(t *testing.T) {
+	logging.SetLogLevel(logging.Info)
+
+	var ssnap SliceSnapshot
+
+	testFunc := func(testErr error, stop bool, injectCompDistErr error, injectCompDistErrOnCout int) {
+		vectorDim := 3
+
+		r := getScanRequest1(vectorDim, 2, []float32{0.4, 1.0, 0.4})
+
+		protoScans := getProtoScans("1")
+		scans, err := r.makeScans(protoScans)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		mcb := codebook.NewMockCodebook(r.IndexInst.Defn.VectorMeta)
+		if injectCompDistErr != nil {
+			mcbImpl := mcb.(*codebook.MockCodebook)
+			mcbImpl.InjectedErr = injectCompDistErr
+			mcbImpl.CompDistErrOnCount = injectCompDistErrOnCout
+		}
+
+		workCh := make(chan *ScanJob)
+		recvCh := make(chan *Row, 10)
+		stopCh := make(chan struct{})
+		errCh := make(chan error, 1)
+		doneCh := make(chan struct{})
+
+		NewScanWorker(1, r, workCh, recvCh, stopCh, errCh, nil)
+
+		var j = ScanJob{
+			pid:      c.PartitionId(0),
+			cid:      0,
+			scan:     scans[0],
+			snap:     ssnap,
+			codebook: mcb,
+			ctx:      nil,
+			doneCh:   doneCh,
+		}
+
+		logging.Infof("Scan: %+v", j.scan)
+
+		workCh <- &j
+
+		go func() {
+			defer close(recvCh)
+			select {
+			case err := <-errCh:
+				if err != testErr {
+					t.Fatalf("Expected: %v Got %v", testErr, err)
+				}
+				logging.Infof("Error received %v", err)
+				return
+			case <-doneCh:
+				logging.Infof("Good Job Done..")
+				return
+			}
+		}()
+
+		if stop {
+			close(stopCh)
+		}
+
+		for row := range recvCh {
+			logging.Infof("Row: %+v", row)
+		}
+	}
+	t.Run("general", func(t *testing.T) {
+		gCount = 0
+		ssnap = getSliceSnapshot1(getVectorDataFeeder(false, 0, nil,
+			false, 0, 0, false))
+		testFunc(nil, false, nil, 0)
+		logging.Infof("gCount: %v", gCount)
+	})
+
+	t.Run("storageerror", func(t *testing.T) {
+		gCount = 0
+		testErr := fmt.Errorf("test injected storage error")
+		ssnap = getSliceSnapshot1(getVectorDataFeeder(true, 70, testErr,
+			false, 0, 0, false))
+		testFunc(testErr, false, nil, 0)
+		logging.Infof("gCount: %v", gCount)
+	})
+
+	t.Run("codebookerror", func(t *testing.T) {
+		gCount = 0
+		testErr := fmt.Errorf("test injected codebook error")
+		ssnap = getSliceSnapshot1(getVectorDataFeeder(false, 0, nil,
+			false, 0, 0, false))
+		testFunc(testErr, false, testErr, 10)
+		logging.Infof("gCount: %v", gCount)
+	})
+
+	t.Run("stop", func(t *testing.T) {
+		gCount = 0
+		ssnap = getSliceSnapshot1(getVectorDataFeeder(false, 0, nil,
+			true, 00, 2*time.Second, false))
+		testFunc(nil, true, nil, 0)
+		logging.Infof("gCount: %v", gCount)
+	})
+}
