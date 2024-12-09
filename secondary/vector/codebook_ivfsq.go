1c3dd4c61b75bb0d8cf86f9e7fb8d5a10c069e09
Vedant Singh <vedant.singh@couchbase.com>
2024-12-09 14:29:09 +0530

    MB-63816: Bound cosine similarity to valid range
    - Floating-point precision errors can cause vectors to be not normalised
    properly, causing cosine similarity values to exceed the valid range of
    [-1, 1].
    - Values will now be clipped to the valid range to prevent invalid
    results.
    
    Change-Id: Id24d47da42dc42975283373e366441d73f40f8fa

--- secondary/vector/codebook_ivfsq.go
+++ secondary/vector/codebook_ivfsq.go
@@ -252,6 +252,9 @@ func (cb *codebookIVFSQ) ComputeDistance(qvec []float32, fvecs []float32, dist [
 			// while cosine distance ranges from 0 (exactly the same) to 2 (exactly opposite).
 			if err == nil {
 				for i := range dist {
+					// Done to prevent floating-point precision errors from producing values
+					// slightly outside the valid [-1, 1] range.
+					dist[i] = clip(dist[i], -1, 1)
 					dist[i] = 1 - dist[i]
 				}
 			}
