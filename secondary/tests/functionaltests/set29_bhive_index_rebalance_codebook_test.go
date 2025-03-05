6d7105f2093509cd84727fc724f1a31a85eb8a48
Shivansh Rustagi <shivansh.rustagi@couchbase.com>
2025-03-05 14:19:07 +0530

    MB-65206: Add Bhive File Based Rebalance Tests
    The patch adds file based rebalance test for bhive indexes.
    At the end of the Rebalance, the codebook directory cleanup is also
    checked, as well as scans are issued to check for recall
    
    Change-Id: Ifd1ef14cc7d0fa33d020cc106e672c4838d78b48

--- secondary/tests/functionaltests/set29_bhive_index_rebalance_codebook_test.go
+++ secondary/tests/functionaltests/set29_bhive_index_rebalance_codebook_test.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"testing"
+	"time"
 
 	"github.com/couchbase/indexing/secondary/testcode"
 	"github.com/couchbase/indexing/secondary/tests/framework/clusterutility"
@@ -14,7 +15,7 @@ import (
 	"github.com/couchbase/indexing/secondary/tests/framework/secondaryindex"
 )
 
-var BHIVE_INDEX_INDEXER_QUOTA = "1024"
+var BHIVE_INDEX_INDEXER_QUOTA = "750"
 
 var idxBhiveDedicated = "idxBHIVE_dedicated_partn"
 var idxBhiveShared = "idxBHIVE_shared_partn"
@@ -29,7 +30,7 @@ func TestBhiveIndexDCPRebalance(t *testing.T) {
 		TestRebalanceSetupCluster(subt)
 		vectorsLoaded = false
 
-		err := clusterutility.SetDataAndIndexQuota(clusterconfig.Nodes[0], clusterconfig.Username, clusterconfig.Password, "1500", "1024")
+		err := clusterutility.SetDataAndIndexQuota(clusterconfig.Nodes[0], clusterconfig.Username, clusterconfig.Password, "1500", BHIVE_INDEX_INDEXER_QUOTA)
 		tc.HandleError(err, "Failed to set memory quota in cluster")
 
 		// wait for indexer to come up as the above step will cause a restart
@@ -184,6 +185,333 @@ func TestBhiveIndexDCPRebalance(t *testing.T) {
 			subt.Fatalf("%v expected rebalance to fail due to failed training but rebalance completed successfully", subt.Name())
 		}
 
+	})
+}
+
+func TestBhiveIndexShardRebalance(t *testing.T) {
+	skipIfNotPlasma(t)
+
+	t.Run("RebalanceSetupCluster", func(subt *testing.T) {
+		TestRebalanceSetupCluster(subt)
+		vectorsLoaded = false
+
+		err := clusterutility.SetDataAndIndexQuota(clusterconfig.Nodes[0], clusterconfig.Username, clusterconfig.Password, "1500", BHIVE_INDEX_INDEXER_QUOTA)
+		tc.HandleError(err, "Failed to set memory quota in cluster")
+
+		// wait for indexer to come up as the above step will cause a restart
+		err = secondaryindex.WaitTillAllIndexNodesActive(kvaddress, defaultIndexActiveTimeout)
+		tc.HandleError(err, fmt.Sprintf("The indexer nodes didnt become active, err:%v", err))
+
+		configChanges := map[string]interface{}{
+			"indexer.settings.enable_shard_affinity": true,
+			"indexer.planner.honourNodesInDefn":      true,
+		}
+		err = secondaryindex.ChangeMultipleIndexerSettings(configChanges, clusterconfig.Username, clusterconfig.Password, clusterconfig.Nodes[1])
+		tc.HandleError(err, fmt.Sprintf("Failed to change config %v", configChanges))
+
+	})
+
+	t.Run("LoadVectorDocsBeforeRebalance", func(subt *testing.T) {
+
+		// Any index created on default scope and collection will be a dedicated instance
+		vectorSetup(subt, BUCKET, "", "", 10000)
+
+		log.Printf("******** Create vector docs on scope and collection **********")
+
+		manifest := kvutility.CreateCollection(BUCKET, scope, coll, clusterconfig.Username, clusterconfig.Password, kvaddress)
+		kvutility.GetCollectionID(BUCKET, scope, coll, clusterconfig.Username, clusterconfig.Password, kvaddress)
+		kvutility.WaitForCollectionCreation(BUCKET, scope, coll, clusterconfig.Username, clusterconfig.Password, []string{kvaddress}, manifest)
+
+		e := loadVectorData(subt, BUCKET, scope, coll, 10000)
+		FailTestIfError(e, "Error in loading vector data", t)
+	})
+
+	t.Run("CreateBhiveVectorIndexesBeforeRebalance", func(subt *testing.T) {
+
+		stmt := fmt.Sprintf("CREATE VECTOR INDEX %v"+
+			" ON `%v`(sift VECTOR)"+
+			" PARTITION BY HASH(meta().id)"+
+			" WITH { \"dimension\":128, \"description\": \"IVF256,PQ32x8\", \"similarity\":\"L2_SQUARED\", \"num_partition\":3, \"defer_build\":true};",
+			idxBhiveDedicated, BUCKET)
+		err := createWithDeferAndBuild(idxBhiveDedicated, BUCKET, "", "", stmt, bhiveIndexActiveTimeout)
+		FailTestIfError(err, "Error in creating "+idxBhiveDedicated, t)
+
+		// Any index created on non-default scope and coll will be a shared instance
+		log.Printf("********Create bhive indices on scope and collection**********")
+
+		stmt = fmt.Sprintf("CREATE VECTOR INDEX %v"+
+			" ON `%v`.`%v`.`%v`(sift VECTOR)"+
+			" PARTITION BY HASH(meta().id)"+
+			" WITH { \"dimension\":128, \"description\": \"IVF256,PQ32x8\", \"similarity\":\"L2_SQUARED\", \"num_partition\":3, \"defer_build\":true};",
+			idxBhiveShared, BUCKET, scope, coll)
+		err = createWithDeferAndBuild(idxBhiveShared, BUCKET, scope, coll, stmt, bhiveIndexActiveTimeout)
+		FailTestIfError(err, "Error in creating "+idxBhiveShared, t)
+	})
+
+	defer t.Run("RebalanceResetCluster", func(subt *testing.T) {
+		TestRebalanceResetCluster(subt)
+
+		configChanges := map[string]interface{}{
+			"indexer.settings.enable_shard_affinity": false,
+			"indexer.planner.honourNodesInDefn":      false,
+			"indexer.shardRebalance.execTestAction":  false,
+		}
+		err := secondaryindex.ChangeMultipleIndexerSettings(configChanges, clusterconfig.Username, clusterconfig.Password, clusterconfig.Nodes[1])
+		tc.HandleError(err, fmt.Sprintf("Failed to change config %v", configChanges))
+	})
+
+	// entry cluster config - [0: kv n1ql] [1: index]
+	// exit cluster config - [0: kv n1ql] [1: index] [2: index] [3: index]
+	t.Run("TestAddTwoNodesAndRebalanceIn", func(subt *testing.T) {
+		addTwoNodesAndRebalance("AddTwoNodesAndRebalanceIn", t)
+		waitForRebalanceCleanup()
+
+		report, err := getLastRebalanceReport(kvaddress, clusterconfig.Username,
+			clusterconfig.Password)
+		tc.HandleError(err, "Failed to get last rebalance report")
+		if completionMsg, exists := report["completionMessage"]; exists &&
+			!strings.Contains(completionMsg.(string), "Rebalance completed successfully.") {
+			subt.Fatalf("Expected rebalance to be successful- %v",
+				report)
+		} else if !exists {
+			subt.Fatalf("Rebalance report does not have any completion message - %v",
+				report)
+		}
+
+		indexStatusResp := performCodebookTransferValidation(subt, []string{idxBhiveDedicated, idxBhiveShared})
+		performStagingCleanupValidation(subt)
+		validateBhiveScan(subt, indexStatusResp)
+	})
+
+	t.Run("TestIndexNodeRebalanceOut", func(subt *testing.T) {
+		TestIndexNodeRebalanceOut(subt)
+
+		indexStatusResp := performCodebookTransferValidation(subt, []string{idxBhiveDedicated, idxBhiveShared})
+		performStagingCleanupValidation(subt)
+		validateBhiveScan(subt, indexStatusResp)
+	})
+
+	t.Run("TestFailoverAndRebalance", func(subt *testing.T) {
+		TestFailoverAndRebalance(subt)
+
+		indexStatusResp := performCodebookTransferValidation(subt, []string{idxBhiveDedicated, idxBhiveShared})
+		performStagingCleanupValidation(subt)
+		validateBhiveScan(subt, indexStatusResp)
+	})
+
+	t.Run("TestSwapRebalance", func(subt *testing.T) {
+		TestSwapRebalance(t)
+
+		indexStatusResp := performCodebookTransferValidation(subt, []string{idxBhiveDedicated, idxBhiveShared})
+		performStagingCleanupValidation(subt)
+		validateBhiveScan(subt, indexStatusResp)
+	})
+
+	// entry and exit cluster config - [0: kv n1ql] [1: index] [2: index]
+	t.Run("TestRebalanceReplicaRepair", func(subt *testing.T) {
+		log.Print("In TestRebalanceReplicaRepair")
+		// Due to previous cluster config, node[3] is still part of the cluster
+		swapRebalance(t, 2, 3)
+
+		status := getClusterStatus()
+		if len(status) != 3 || !isNodeIndex(status, clusterconfig.Nodes[1]) ||
+			!isNodeIndex(status, clusterconfig.Nodes[2]) {
+			subt.Fatalf("%v Unexpected cluster configuration: %v", subt.Name(), status)
+		}
+
+		printClusterConfig(subt.Name(), "entry")
+
+		// Create Replicated Vector Instances - shared and dedicated
+		// NOTE: don't drop these indexes as next test will use these
+		stmt := fmt.Sprintf("CREATE VECTOR INDEX %v"+
+			" ON `%v`(sift VECTOR)"+
+			" WITH { \"dimension\":128, \"description\": \"IVF256,PQ32x8\", \"similarity\":\"L2_SQUARED\", \"num_replica\":1, \"defer_build\":true};",
+			idxBhiveDedicatedReplica, BUCKET)
+		err := createWithDeferAndBuild(idxBhiveDedicatedReplica, BUCKET, "", "", stmt, bhiveIndexActiveTimeout)
+		FailTestIfError(err, "Error in creating "+idxBhiveDedicatedReplica, subt)
+
+		startTime := time.Now()
+		stmt = fmt.Sprintf("CREATE VECTOR INDEX %v"+
+			" ON `%v`.`%v`.`%v`(sift VECTOR)"+
+			" WITH { \"dimension\":128, \"description\": \"IVF256,PQ32x8\", \"similarity\":\"L2_SQUARED\", \"num_replica\":1, \"defer_build\":true};",
+			idxBhiveSharedReplica, BUCKET, scope, coll)
+		err = createWithDeferAndBuild(idxBhiveSharedReplica, BUCKET, scope, coll, stmt, bhiveIndexActiveTimeout)
+		FailTestIfError(err, "Error in creating "+idxBhiveSharedReplica, subt)
+		timeTaken := time.Since(startTime)
+
+		time.Sleep(timeTaken / 2)
+
+		// Failover Node 2 - Replica is lost
+		log.Printf("%v: Failing over index node %v", subt.Name(), clusterconfig.Nodes[2])
+		failoverNode(clusterconfig.Nodes[2], t)
+		log.Printf("%v: Rebalancing", subt.Name())
+		rebalance(t)
+
+		// Add Node 2 and Rebalance - Replica is repaired
+		log.Printf("%v: Add Node %v to the cluster for replica repair", subt.Name(), clusterconfig.Nodes[2])
+		addNodeAndRebalance(clusterconfig.Nodes[2], "index", t)
+
+		waitForRebalanceCleanup()
+
+		indexStatusResp := performCodebookTransferValidation(subt, []string{idxBhiveDedicated, idxBhiveShared})
+		validateBhiveScan(subt, indexStatusResp)
+	})
+
+	// NOTE: The replicated index from the previous test ensure that atleast one Index is
+	// present on the NodeOut
+	// entry and exit cluster config - [0: kv n1ql] [1: index] [2: index]
+	t.Run("TestRebalanceCancelIndexerAfterTransfer", func(subt *testing.T) {
+
+		log.Print("In TestRebalanceCancelIndexerAfterTransfer")
+		status := getClusterStatus()
+		if len(status) != 3 || !isNodeIndex(status, clusterconfig.Nodes[1]) ||
+			!isNodeIndex(status, clusterconfig.Nodes[2]) {
+			subt.Fatalf("%v Unexpected cluster configuration: %v", subt.Name(), status)
+		}
+
+		printClusterConfig(subt.Name(), "entry")
+
+		log.Print("** Setting TestAction REBALANCE_CANCEL for SOURCE_SHARDTOKEN_AFTER_TRANSFER")
+		configChanges := map[string]interface{}{
+			"indexer.shardRebalance.execTestAction": true,
+		}
+		err := secondaryindex.ChangeMultipleIndexerSettings(configChanges, clusterconfig.Username, clusterconfig.Password, clusterconfig.Nodes[1])
+		tc.HandleError(err, "Failed to activate testactions")
+
+		defer func() {
+			waitForRebalanceCleanup()
+			configChanges := map[string]interface{}{
+				"indexer.shardRebalance.execTestAction": false,
+			}
+			err := secondaryindex.ChangeMultipleIndexerSettings(configChanges, clusterconfig.Username, clusterconfig.Password, clusterconfig.Nodes[1])
+			tc.HandleError(err, "Failed to activate testactions")
+
+			removeNode(clusterconfig.Nodes[3], subt)
+			printClusterConfig(subt.Name(), "exit")
+		}()
+
+		tag := testcode.SOURCE_SHARDTOKEN_AFTER_TRANSFER
+		err = testcode.PostOptionsRequestToMetaKV(clusterconfig.Nodes[2], clusterconfig.Username,
+			clusterconfig.Password, tag, testcode.REBALANCE_CANCEL, "", 0)
+		FailTestIfError(err, "Error while posting request to metaKV", subt)
+
+		swapAndCheckForCleanup(subt, 3, 2)
+	})
+
+	// entry and exit cluster config - [0: kv n1ql] [1: index] [2: index]
+	t.Run("TestRebalanceCancelIndexerAfterRestore", func(subt *testing.T) {
+
+		log.Print("In TestRebalanceCancelIndexerAfterRestore")
+		status := getClusterStatus()
+		if len(status) != 3 || !isNodeIndex(status, clusterconfig.Nodes[1]) ||
+			!isNodeIndex(status, clusterconfig.Nodes[2]) {
+			subt.Fatalf("%v Unexpected cluster configuration: %v", subt.Name(), status)
+		}
+
+		printClusterConfig(subt.Name(), "entry")
+
+		log.Print("** Setting TestAction REBALANCE_CANCEL for DEST_SHARDTOKEN_AFTER_RESTORE")
+		configChanges := map[string]interface{}{
+			"indexer.shardRebalance.execTestAction": true,
+		}
+		err := secondaryindex.ChangeMultipleIndexerSettings(configChanges, clusterconfig.Username, clusterconfig.Password, clusterconfig.Nodes[1])
+		tc.HandleError(err, "Failed to activate testactions")
+
+		defer func() {
+			waitForRebalanceCleanup()
+			configChanges := map[string]interface{}{
+				"indexer.shardRebalance.execTestAction": false,
+			}
+			err := secondaryindex.ChangeMultipleIndexerSettings(configChanges, clusterconfig.Username, clusterconfig.Password, clusterconfig.Nodes[1])
+			tc.HandleError(err, "Failed to activate testactions")
+
+			removeNode(clusterconfig.Nodes[3], subt)
+			printClusterConfig(subt.Name(), "exit")
+		}()
+
+		tag := testcode.DEST_SHARDTOKEN_AFTER_RESTORE
+		err = testcode.PostOptionsRequestToMetaKV(clusterconfig.Nodes[3], clusterconfig.Username,
+			clusterconfig.Password, tag, testcode.REBALANCE_CANCEL, "", 0)
+		FailTestIfError(err, "Error while posting request to metaKV", subt)
+
+		swapAndCheckForCleanup(subt, 3, 2)
+	})
+
+	// entry and exit cluster config - [0: kv n1ql] [1: index] [2: index]
+	t.Run("TestRebalanceCancelIndexerBeforeRecovery", func(subt *testing.T) {
+
+		log.Print("In TestRebalanceCancelIndexerBeforeRecovery")
+		status := getClusterStatus()
+		if len(status) != 3 || !isNodeIndex(status, clusterconfig.Nodes[1]) ||
+			!isNodeIndex(status, clusterconfig.Nodes[2]) {
+			subt.Fatalf("%v Unexpected cluster configuration: %v", subt.Name(), status)
+		}
+
+		printClusterConfig(subt.Name(), "entry")
+
+		log.Print("** Setting TestAction REBALANCE_CANCEL for DEST_INDEXER_BEFORE_INDEX_RECOVERY")
+		configChanges := map[string]interface{}{
+			"indexer.shardRebalance.execTestAction": true,
+		}
+		err := secondaryindex.ChangeMultipleIndexerSettings(configChanges, clusterconfig.Username, clusterconfig.Password, clusterconfig.Nodes[1])
+		tc.HandleError(err, "Failed to activate testactions")
+
+		defer func() {
+			waitForRebalanceCleanup()
+			configChanges := map[string]interface{}{
+				"indexer.shardRebalance.execTestAction": false,
+			}
+			err := secondaryindex.ChangeMultipleIndexerSettings(configChanges, clusterconfig.Username, clusterconfig.Password, clusterconfig.Nodes[1])
+			tc.HandleError(err, "Failed to activate testactions")
+
+			removeNode(clusterconfig.Nodes[3], subt)
+			printClusterConfig(subt.Name(), "exit")
+		}()
+
+		tag := testcode.DEST_INDEXER_BEFORE_INDEX_RECOVERY
+		err = testcode.PostOptionsRequestToMetaKV(clusterconfig.Nodes[3], clusterconfig.Username,
+			clusterconfig.Password, tag, testcode.REBALANCE_CANCEL, "", 0)
+		FailTestIfError(err, "Error while posting request to metaKV", subt)
+
+		swapAndCheckForCleanup(subt, 3, 2)
+	})
+
+	// entry and exit cluster config - [0: kv n1ql] [1: index] [2: index]
+	t.Run("TestRebalanceCancelIndexerAfterRecovery", func(subt *testing.T) {
+		log.Print("In TestRebalanceCancelIndexerAfterRecovery")
+		status := getClusterStatus()
+		if len(status) != 3 || !isNodeIndex(status, clusterconfig.Nodes[1]) ||
+			!isNodeIndex(status, clusterconfig.Nodes[2]) {
+			subt.Fatalf("%v Unexpected cluster configuration: %v", subt.Name(), status)
+		}
+
+		printClusterConfig(subt.Name(), "entry")
+
+		log.Print("** Setting TestAction REBALANCE_CANCEL for DEST_INDEXER_AFTER_INDEX_RECOVERY")
+		configChanges := map[string]interface{}{
+			"indexer.shardRebalance.execTestAction": true,
+		}
+		err := secondaryindex.ChangeMultipleIndexerSettings(configChanges, clusterconfig.Username, clusterconfig.Password, clusterconfig.Nodes[1])
+		tc.HandleError(err, "Failed to activate testactions")
+
+		defer func() {
+			waitForRebalanceCleanup()
+			configChanges := map[string]interface{}{
+				"indexer.shardRebalance.execTestAction": false,
+			}
+			err := secondaryindex.ChangeMultipleIndexerSettings(configChanges, clusterconfig.Username, clusterconfig.Password, clusterconfig.Nodes[1])
+			tc.HandleError(err, "Failed to activate testactions")
+
+			removeNode(clusterconfig.Nodes[3], subt)
+			printClusterConfig(subt.Name(), "exit")
+		}()
+
+		tag := testcode.DEST_INDEXER_AFTER_INDEX_RECOVERY
+		err = testcode.PostOptionsRequestToMetaKV(clusterconfig.Nodes[3], clusterconfig.Username,
+			clusterconfig.Password, tag, testcode.REBALANCE_CANCEL, "", 0)
+		FailTestIfError(err, "Error while posting request to metaKV", subt)
+
+		swapAndCheckForCleanup(subt, 3, 2)
 	})
 }
 
