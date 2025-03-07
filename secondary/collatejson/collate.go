df00a711668955feb881bf8df58555f57b0e7604
Varun Velamuri <varun.velamuri@couchbase.com>
2025-03-08 00:48:18 +0530

    MB-65203 Generate n1qlVal based on code instead of tmp buffer
    As tmp buffer can get overridden with data. When there are more
    than one string entries in code, then the first and second
    entries can potentially share the same data due to the way the
    value is constructed. This can lead to incorrect results.
    
    Instead, the value is generated based on "code" as code does
    not change throughout the processing of row.
    
    Change-Id: I73c15388babb0008aa0e8368fbac2efb3c732a73

--- secondary/collatejson/collate.go
+++ secondary/collatejson/collate.go
@@ -1038,7 +1038,7 @@ func (codec *Codec) code2n1qlpooled(code, text []byte, decode bool, pos int, svP
 		var strb []byte
 		strb, remaining, err = suffixDecodeString(code[1:], text)
 		if decode && err == nil {
-			n1qlVal = svPool.Get(pos, conversions.ByteSliceToString(strb))
+			n1qlVal = svPool.Get(pos, conversions.ByteSliceToString(code[1:1+len(strb)]))
 		}
 		return n1qlVal, remaining, err
 	default:
