fa90d82365b60e64d474088f1303bdfd2f754b30
deepkaran <deepkaran@couchbase.com>
2026-01-27 13:41:48 -0800

    MB-70200 Add helper for sparse vector generation
    Change-Id: I1fc54584516e7e62c14fe5cd0e79a9fe9fa50b97

--- secondary/vector/utils.go
+++ secondary/vector/utils.go
@@ -2,6 +2,7 @@ package vector
 
 import (
 	"math/rand"
+	"sort"
 	"time"
 )
 
@@ -11,13 +12,62 @@ func genRandomVecs(dims int, n int, seed int64) [][]float32 {
 	} else {
 		rand.Seed(time.Now().UnixNano())
 	}
-	
+
 	vecs := make([][]float32, n)
 	for i := 0; i < n; i++ {
 		vecs[i] = make([]float32, dims)
 		for j := 0; j < dims; j++ {
 			vecs[i][j] = rand.Float32()
 		}
+	}
+
+	return vecs
+}
+
+func genRandomSparseVecs(size int, n int, seed int64) [][]float32 {
+	if seed != 0 {
+		rand.Seed(seed)
+	} else {
+		rand.Seed(time.Now().UnixNano())
+	}
+
+	if size < 0 {
+		size = 0
+	}
+	if size > 30000 {
+		size = 30000
+	}
+
+	vecs := make([][]float32, n)
+	for i := 0; i < n; i++ {
+		vec := make([]float32, 1+(2*size))
+		vec[0] = float32(size)
+
+		positions := make([]int, 0, size)
+		if size > 0 {
+			const maxPos = 30000
+			chosen := make(map[int]struct{}, size)
+			for j := maxPos - size + 1; j <= maxPos; j++ {
+				t := rand.Intn(j) + 1
+				if _, ok := chosen[t]; ok {
+					chosen[j] = struct{}{}
+				} else {
+					chosen[t] = struct{}{}
+				}
+			}
+			for pos := range chosen {
+				positions = append(positions, pos)
+			}
+		}
+		sort.Ints(positions)
+		for j := 0; j < size; j++ {
+			vec[1+j] = float32(uint32(positions[j] + 1))
+		}
+		for j := 0; j < size; j++ {
+			vec[1+size+j] = rand.Float32()
+		}
+
+		vecs[i] = vec
 	}
 
 	return vecs
