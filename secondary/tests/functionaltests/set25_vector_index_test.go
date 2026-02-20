0881f6e6823dcd95ae24bb02fb2944ac10dbad56
Shivansh Rustagi <shivansh.rustagi@couchbase.com>
2026-02-20 21:25:33 +0530

    MB-70189: Add index items count test for Bhive Sparse Vector
    Add a sanity test to check correct processing of the mutations, and
    insertion in bhive sparse vector by checking the expected items count in
    the index
    
    Change-Id: I6f299e52aa195c5b7ca74ff7087f2fa82148b530

--- secondary/tests/functionaltests/set25_vector_index_test.go
+++ secondary/tests/functionaltests/set25_vector_index_test.go
@@ -27,6 +27,8 @@ var idx_sif10k_partn = "idx_sift10k_partn"
 var idx_base64 = "idx_base64"
 var vectorIndexActiveTimeout = int64(180) //3 min
 
+const SPARSE_DIMENSION int = 128
+
 // Fist queryvector from SIFT10K
 var indexVector = &datastore.IndexVector{
 	QueryVector: []float32{1, 3, 11, 110, 62, 22, 4, 0, 43, 21, 22, 18, 6, 28, 64, 9, 11, 1, 0, 0, 1, 40, 101, 21, 20, 2, 4, 2, 2, 9, 18, 35, 1, 1, 7, 25, 108, 116, 63, 2, 0, 0, 11, 74, 40, 101, 116, 3, 33, 1, 1, 11, 14, 18, 116, 116, 68, 12, 5, 4, 2, 2, 9, 102, 17, 3, 10, 18, 8, 15, 67, 63, 15, 0, 14, 116, 80, 0, 2, 22, 96, 37, 28, 88, 43, 1, 4, 18, 116, 51, 5, 11, 32, 14, 8, 23, 44, 17, 12, 9, 0, 0, 19, 37, 85, 18, 16, 104, 22, 6, 2, 26, 12, 58, 67, 82, 25, 12, 2, 2, 25, 18, 8, 2, 19, 42, 48, 11},
@@ -36,10 +38,16 @@ var indexVector = &datastore.IndexVector{
 // Top100 Nearest 0 based vector positions for above query vector from SIFT10K dataset
 var expectedVectorPosTop100 = []uint32{2176, 3752, 882, 4009, 2837, 190, 3615, 816, 1045, 1884, 224, 3013, 292, 1272, 5307, 4938, 1295, 492, 9211, 3625, 1254, 1292, 1625, 3553, 1156, 146, 107, 5231, 1995, 9541, 3543, 9758, 9806, 1064, 9701, 4064, 2456, 2763, 3237, 1317, 3530, 641, 1710, 8887, 4263, 1756, 598, 370, 2776, 121, 4058, 7245, 1895, 124, 8731, 696, 4320, 4527, 4050, 2648, 1682, 2154, 1689, 2436, 2005, 3210, 4002, 2774, 924, 6630, 3449, 9814, 3515, 5375, 287, 1038, 4096, 4094, 942, 4321, 123, 3814, 97, 4293, 420, 9734, 1916, 2791, 149, 6139, 9576, 6837, 2952, 3138, 2890, 3066, 2852, 348, 3043, 3687}
 
-var vectorsLoaded = false
+var vectorsLoaded = false       // represents the dense vector
+var sparseVectorsLoaded = false // represents the sparse vector
 var vecIndexCreated = false
 var vecPartnIndexCreated = false
 var multiIndexerConfig = false
+
+func resetVectorDataSetupFlags() {
+	vectorsLoaded = false
+	sparseVectorsLoaded = false
+}
 
 // Load Data
 // Dataset uses SIFT10K Small and repeats this 10K multifold by adding scalar fields with different cardinality
@@ -64,6 +72,7 @@ func vectorSetup(t *testing.T, bucket, scope, coll string, numDocs int) {
 	FailTestIfError(e, "Error in DropAllSecondaryIndexes", t)
 
 	kv.FlushBucket("default", "", clusterconfig.Username, clusterconfig.Password, kvaddress)
+	resetVectorDataSetupFlags()
 
 	e = loadVectorData(t, bucket, scope, coll, numDocs)
 	FailTestIfError(e, "Error in loading vector data", t)
@@ -95,6 +104,50 @@ func loadVectorData(t *testing.T, bucket, scope, coll string, numDocs int) error
 		UseSIFTSmall:   true,
 		SkipNormalData: true,
 		SIFTFVecsFile:  "../../tools/randdocs/siftsmall/siftsmall_base.fvecs",
+	}
+	return randdocs.Run(cfg)
+}
+
+func vectorSetupSparse(t *testing.T, bucket, scope, coll string, numDocs int) {
+	skipIfNotPlasma(t)
+
+	// Drop all indexes from earlier tests
+	e := secondaryindex.DropAllSecondaryIndexes(indexManagementAddress)
+	FailTestIfError(e, "Error in DropAllSecondaryIndexes", t)
+
+	kv.FlushBucket("default", "", clusterconfig.Username, clusterconfig.Password, kvaddress)
+	resetVectorDataSetupFlags()
+
+	e = loadSparseVectorData(t, bucket, scope, coll, numDocs)
+	FailTestIfError(e, "Error in loading sparse vector data", t)
+
+	sparseVectorsLoaded = true
+}
+
+func loadSparseVectorData(t *testing.T, bucket, scope, coll string, numDocs int) error {
+	if scope == "" {
+		scope = c.DEFAULT_SCOPE
+	}
+	if coll == "" {
+		coll = c.DEFAULT_COLLECTION
+	}
+	if numDocs == 0 {
+		numDocs = 10000
+	}
+
+	cfg := randdocs.Config{
+		ClusterAddr:      "127.0.0.1:9000",
+		Bucket:           bucket,
+		Scope:            scope,
+		Collection:       coll,
+		NumDocs:          numDocs,
+		DocIdLen:         25,
+		Iterations:       1,
+		Threads:          8,
+		OpsPerSec:        100000,
+		SkipNormalData:   false,
+		GenSparseVectors: true,
+		SparseVecDim:     SPARSE_DIMENSION,
 	}
 	return randdocs.Run(cfg)
 }
