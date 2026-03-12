72365bb1d75fe30af7871a4976b454f8612d75f0
Dhruvil Shah <dsdruvil8@gmail.com>
2026-03-12 10:10:52 +0530

    MB-100 update URL query param to match cbcollect
    when merging changes for cbcollect, we have updated the URL
    param to store-stats instead of debug as that was very generic;
    this patch is to additionally handle "store-stats" as well
    as debug;
    
    Change-Id: I5f3f3cb1e74dc8f08f12a6be6db0bcb29262c546
    Signed-off-by: Dhruvil Shah <dsdruvil8@gmail.com>

--- secondary/indexer/stats_manager.go
+++ secondary/indexer/stats_manager.go
@@ -5136,7 +5136,12 @@ func (s *statsManager) handleMetaStatsRequest(w http.ResponseWriter, r *http.Req
 		r.ParseForm()
 
 		// /stats/metadata?debug=1
+		// OR
+		// /stats/metadata?store-stats=1
 		dbgStr := r.FormValue("debug")
+		storeStr := r.FormValue("store-stats")
+
+		var shouldWriteStoreSts = dbgStr == "1" || storeStr == "1"
 
 		var metastats, ok = s.getMetadataStats()
 
@@ -5156,7 +5161,7 @@ func (s *statsManager) handleMetaStatsRequest(w http.ResponseWriter, r *http.Req
 			return
 		}
 
-		if dbgStr == "1" {
+		if shouldWriteStoreSts {
 			// if debug, also send raw stats
 			var rawStats, err1 = json.Marshal(metastats.Raw) // Raw is expected to be a map => {...}
 			if err1 == nil && len(rawStats) > 2 {
